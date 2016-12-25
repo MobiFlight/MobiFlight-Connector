7966033cbac303086aea9e480d33ac932df6ef49
DocMoebiuz <bitbucket@moebiuz.de>
2016-12-25 14:28:49 -0600

    Automated Release of Release-Build with PostBuild Jobs

--- Build/release.bat
+++ Build/release.bat
@@ -1,7 +1,16 @@
 @ECHO OFF
 SET RELEASE_DIR=%1
-SET VERSION=%2
+SET SOLUTION_DIR=%2
+SET ICONFILE=%SOLUTION_DIR%\mobiflight.ico
 SET BUILD_TOOLS_DIR=%~dp0
-SET ICONFILE=..\mobiflight.ico
+SET VERSION=
+for /f "delims=" %%A in ('%BUILD_TOOLS_DIR%VersionInfo.exe %RELEASE_DIR%MFConnector.exe') do set "VERSION=%%A"
 
-echo %BUILD_TOOLS_DIR%rar.exe a -o+ -sfx -ep1 -r ..\Release\MobiFlightConnector-%VERSION% -zsfx-mobiflight.diz -iiconc:%ICONFILE% %RELEASE_DIR%\*.*
+echo -----------------------------------------------------------
+echo Running command to create release package version %VERSION%
+echo -----------------------------------------------------------
+echo %BUILD_TOOLS_DIR%rar.exe a -o+ -sfx -ep1 -r %SOLUTION_DIR%Release\MobiFlightConnector-%VERSION% -z%BUILD_TOOLS_DIR%sfx-mobiflight.diz -iiconc:%ICONFILE% %RELEASE_DIR%*.*
+%BUILD_TOOLS_DIR%rar.exe a -o+ -sfx -ep1 -r %SOLUTION_DIR%Release\MobiFlightConnector-%VERSION% -z%BUILD_TOOLS_DIR%sfx-mobiflight.diz -iiconc:%ICONFILE% %RELEASE_DIR%*.*
+echo -----------------------------------------------------------
+echo DONE
+echo -----------------------------------------------------------
---
+++ VersionInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace VersionInfo
+{
+    class VersionInfo
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args.Length > 1) ShowUsage();
+
+            string target = args[0];
+
+            string path = Path.IsPathRooted(target)
+                                ? target
+                                : Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + Path.DirectorySeparatorChar + target;
+
+            Console.Write(Assembly.LoadFile(path).GetName().Version.ToString(3));
+        }
+
+        static void ShowUsage()
+        {
+            Console.WriteLine("Usage: versioninfo.exe <target>");
+        }
+    }
+}
