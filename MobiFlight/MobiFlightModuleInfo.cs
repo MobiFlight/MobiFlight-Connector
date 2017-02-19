a9c995469da51dc4a1ea8ea0afacb73d1d689734
DocMoebiuz <DocMoebiuz@bitbucket.org>
2017-02-18 19:25:11 -0600

    Move PID from UNO back to MEGA

--- MobiFlight/MobiFlightModuleInfo.cs
+++ MobiFlight/MobiFlightModuleInfo.cs
@@ -80,7 +80,7 @@ namespace MobiFlight
         };
 
         public static readonly String[] VIDPID_UNO = {
-            "VID_1A86&PID_7523",
+            //"VID_1A86&PID_7523",
             "VID_2341&PID_0043"
         };
 
@@ -88,7 +88,7 @@ namespace MobiFlight
             "VID_2341&PID_0042",
             "VID_2341&PID_0010",
             "VID_8087&PID_0024",
-            //"VID_1A86&PID_7523", // this is actually an CH-340 and can be a Mega OR an UNO
+            "VID_1A86&PID_7523", // this is actually an CH-340 and can be a Mega OR an UNO
             "VID_2A03&PID_0042", // http://www.mobiflight.de/forum/message/983.html
             "VID_0403&PID_6001"  // http://www.mobiflight.de/forum/topic/570.html
 
