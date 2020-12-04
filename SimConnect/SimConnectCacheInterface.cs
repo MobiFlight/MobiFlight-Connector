037d5612fc647ed91d3e546e8c304230c5569c1a
Sebastian M <bitbucket@moebiuz.de>
2020-12-04 12:29:08 +0100

    started adding SimConnect Support

---
+++ SimConnect/SimConnectCacheInterface.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiFlight.SimConnect
+{
+    class SimConnectCacheInterface
+    {
+        public event EventHandler Closed;
+        public event EventHandler Connected;
+        public event EventHandler ConnectionLost;
+
+       /* public void Clear()
+        {
+            throw new NotImplementedException();
+        }*/
+
+        public bool connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void sendEventID(int eventID, int param)
+        {
+        }
+    }
+}
