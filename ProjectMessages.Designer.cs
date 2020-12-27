30d4866b35092b0dbe68adce401b955793214a6b
Sebastian M <bitbucket@moebiuz.de>
2020-12-27 17:37:08 +0100

    fixed Issue #288

--- ProjectMessages.Designer.cs
+++ ProjectMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace MobiFlight {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class ProjectMessages {
@@ -870,7 +870,7 @@ namespace MobiFlight {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to http://mobiflight.moebiuz.de/dokumentation/software.html.
+        ///   Looks up a localized string similar to https://www.mobiflight.com/en/documentation/software.html.
         /// </summary>
         internal static string WebsiteUrlHelp {
             get {
