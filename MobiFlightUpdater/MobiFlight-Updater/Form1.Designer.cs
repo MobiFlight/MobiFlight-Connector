5184a56453e3a199b04b568301b79d5dfda172a1
Sebastian M <bitbucket@moebiuz.de>
2021-03-27 17:52:47 +0100

    Merged manually changes for updater.

--- MobiFlightUpdater/MobiFlight-Updater/Form1.Designer.cs
+++ MobiFlightUpdater/MobiFlight-Updater/Form1.Designer.cs
@@ -29,53 +29,134 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdaterForm));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ButtonExit = new System.Windows.Forms.Button();
+            this.ButtonInstall = new System.Windows.Forms.Button();
+            this.ListVersions = new System.Windows.Forms.ListBox();
+            this.ButtonDownload = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.StatusCurrent = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ButtonStartMF = new System.Windows.Forms.Button();
+            this.ShowNotes = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // ButtonExit
             // 
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(171, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.ButtonExit.Location = new System.Drawing.Point(211, 411);
+            this.ButtonExit.Name = "ButtonExit";
+            this.ButtonExit.Size = new System.Drawing.Size(75, 49);
+            this.ButtonExit.TabIndex = 0;
+            this.ButtonExit.Text = "Exit";
+            this.ButtonExit.UseVisualStyleBackColor = true;
+            this.ButtonExit.Click += new System.EventHandler(this.ButtonExit_Click);
+            // 
+            // ButtonInstall
+            // 
+            this.ButtonInstall.Location = new System.Drawing.Point(12, 411);
+            this.ButtonInstall.Name = "ButtonInstall";
+            this.ButtonInstall.Size = new System.Drawing.Size(76, 49);
+            this.ButtonInstall.TabIndex = 2;
+            this.ButtonInstall.Text = "INSTALL";
+            this.ButtonInstall.UseVisualStyleBackColor = true;
+            this.ButtonInstall.Click += new System.EventHandler(this.ButtonInstall_Click);
+            // 
+            // ListVersions
+            // 
+            this.ListVersions.FormattingEnabled = true;
+            this.ListVersions.Location = new System.Drawing.Point(16, 139);
+            this.ListVersions.Name = "ListVersions";
+            this.ListVersions.Size = new System.Drawing.Size(270, 251);
+            this.ListVersions.TabIndex = 3;
+            this.ListVersions.SelectedIndexChanged += new System.EventHandler(this.ListVersions_SelectedIndexChanged);
+            // 
+            // ButtonDownload
+            // 
+            this.ButtonDownload.Location = new System.Drawing.Point(88, 81);
+            this.ButtonDownload.Name = "ButtonDownload";
+            this.ButtonDownload.Size = new System.Drawing.Size(108, 39);
+            this.ButtonDownload.TabIndex = 4;
+            this.ButtonDownload.Text = "Download";
+            this.ButtonDownload.UseVisualStyleBackColor = true;
+            this.ButtonDownload.Click += new System.EventHandler(this.ButtonDownload_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 505);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(274, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // StatusCurrent
+            // 
+            this.StatusCurrent.AutoSize = true;
+            this.StatusCurrent.Location = new System.Drawing.Point(33, 489);
+            this.StatusCurrent.Name = "StatusCurrent";
+            this.StatusCurrent.Size = new System.Drawing.Size(0, 13);
+            this.StatusCurrent.TabIndex = 7;
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(0, 65);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Updating";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // ButtonStartMF
+            // 
+            this.ButtonStartMF.Enabled = false;
+            this.ButtonStartMF.Location = new System.Drawing.Point(115, 411);
+            this.ButtonStartMF.Name = "ButtonStartMF";
+            this.ButtonStartMF.Size = new System.Drawing.Size(69, 49);
+            this.ButtonStartMF.TabIndex = 9;
+            this.ButtonStartMF.Text = "Start MobiFlight";
+            this.ButtonStartMF.UseVisualStyleBackColor = true;
+            this.ButtonStartMF.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // ShowNotes
+            // 
+            this.ShowNotes.Location = new System.Drawing.Point(309, 2);
+            this.ShowNotes.MinimumSize = new System.Drawing.Size(20, 20);
+            this.ShowNotes.Name = "ShowNotes";
+            this.ShowNotes.Size = new System.Drawing.Size(664, 539);
+            this.ShowNotes.TabIndex = 10;
             // 
             // UpdaterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(173, 93);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(975, 541);
+            this.Controls.Add(this.ShowNotes);
+            this.Controls.Add(this.ButtonStartMF);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.StatusCurrent);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.ButtonDownload);
+            this.Controls.Add(this.ListVersions);
+            this.Controls.Add(this.ButtonInstall);
+            this.Controls.Add(this.ButtonExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "UpdaterForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MobiFlight Updater";
-            this.Shown += new System.EventHandler(this.UpdaterForm_Shown);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ButtonExit;
+        private System.Windows.Forms.Button ButtonInstall;
+        private System.Windows.Forms.ListBox ListVersions;
+        private System.Windows.Forms.Button ButtonDownload;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label StatusCurrent;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonStartMF;
+        private System.Windows.Forms.WebBrowser ShowNotes;
     }
 }
 
--- MobiFlightUpdater/MobiFlight-Updater/Form1.cs
+++ MobiFlightUpdater/MobiFlight-Updater/Form1.cs
@@ -1,71 +1,232 @@
 ﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Net;
+using System.Net.Cache;
+using System.Threading;
+using System.Xml;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.IO.Compression;
+using System.Security.Cryptography;
 
 namespace MobiFlight_Updater
 {
     public partial class UpdaterForm : Form
     {
+        readonly string InstallerUpdateUrl = ""; // Set to empty to avoid installer autoUpgrade
+        //readonly string MobiFlightUpdateUrlRelease = "https://connectoo.fr/mobiflight_zip_list.xml";
+        public static readonly string MobiFlightUpdateUrl = "https://www.mobiflight.com/tl_files/download/releases/mobiflightconnector.xml";
+        public static readonly string MobiFlightUpdateBetasUrl = "https://www.mobiflight.com/tl_files/download/releases/beta/mobiflightconnector.xml";
+        public static readonly string MobiFlightUpdateDebugUrl = "https://www.mobiflight.com/tl_files/download/releases/debug/mobiflightconnector.xml";
+        string ActualInstalledVersion = "0.0.0";
+        
+        private string _downloadURL = "";
+        private string _downloadChecksum = "";
+        private string _downloadNotes = "";
+        private string _tempPath;
+        private WebClient _webClient;
+        private string CurrentFileName = "";
+
         public UpdaterForm()
         {
-            InitializeComponent();
-        }
+            CmdLineParams cmdParams = new CmdLineParams(Environment.GetCommandLineArgs());
 
-        private async void UpdaterForm_Shown(object sender, EventArgs e)
-        {
-            var result = false;
-            while (!result)
+            // You can use this for static testing.
+            cmdParams = new CmdLineParams(new []{ "MobiFlightUpdater.exe", 
+                                                  "/expert" });
+
+            if (cmdParams.HasParams()) // if args are present
             {
-                result = await PerformUpdate();
+                MobiFlightUpdaterModel.DownloadVersionsList(MobiFlightUpdateUrl);
 
-                if (!result)
+                // install specific version by command line
+                if (cmdParams.Install != null) 
                 {
-                    DialogResult dResult = MessageBox.Show("MobiFlight appears to be running and cannot be stopped. Please stop manually and try the update again.", "Error", MessageBoxButtons.RetryCancel);
-                    if (dResult != DialogResult.Retry) break;
+                    MobiFlightUpdaterModel.ManualUpgradeFromCommandLine(cmdParams.Install);
+                }
+                else if (cmdParams.Check || cmdParams.CheckBeta)
+                {
+                    MobiFlightUpdaterModel.ExternAskToCheckLastVersion(cmdParams.CheckBeta);
+                }
+                // expert mode, start interface to choose version
+                else if (cmdParams.ExpertMode) 
+                {
+                    InitializeComponent();
+                    if (InstallerUpdateUrl != "")
+                        MobiFlightUpdaterModel.InstallerCheckForUpgrade(InstallerUpdateUrl);
+                    ActualInstalledVersion = MobiFlightUpdaterModel.GetActualInstalledVersion();
+                    if (ActualInstalledVersion != "0.0.0")
+                    {
+                        label1.Text = "MobiFlight " + ActualInstalledVersion + " is actually installed !";
+                        ButtonStartMF.Enabled = true;
+                    }
+                    else
+                    {
+                        label1.Text = "Choose wich version do you want to install";
+                        ButtonStartMF.Enabled = false;
+                    }
+                    ButtonInstall.Enabled = false;
+                    if (MobiFlightUpdaterModel.VerifyCurrentFolderRight())
+                    {
+                        MobiFlightUpdaterModel.DownloadVersionsList(MobiFlightUpdateUrl);
+                        PopulateListVersions();
+                    }
+                    else
+                    {
+                        MessageBox.Show("You don't have right on this folder, Mobiflight can't install here !");
+                        Environment.Exit(0);
+                    }
                 }
             }
-
-            if (result) {
-                var proc1 = new ProcessStartInfo();
-                var args = Environment.GetCommandLineArgs();
-                string applicationName = $"MobiFlightSetup-{args[1]}.exe";
-                proc1.UseShellExecute = true;
-                proc1.FileName = applicationName;
-                proc1.WindowStyle = ProcessWindowStyle.Normal;
-                Process p = Process.Start(proc1);
-            }
-
-            this.Close();
-        }
-
-        private async Task<bool> PerformUpdate()
-        {
-            List<Task<bool>> tasks = new List<Task<bool>>();
-            tasks.Add(Task.Run(() =>
+            else
             {
-                System.Threading.Thread.Sleep(5000);
-                return false;
-            }));
-            tasks.Add(Task.Run(() =>
-            {
-                string proc = "MFConnector";
-                while (Process.GetProcessesByName(proc).Length > 0)
+                if (!File.Exists(MobiFlightUpdaterModel.ProcessName)) // direct install last release version if MF don't exist.
                 {
-                    System.Threading.Thread.Sleep(500);
+                    if (MobiFlightUpdaterModel.VerifyCurrentFolderRight())
+                    {
+                        MobiFlightUpdaterModel.DownloadVersionsList(MobiFlightUpdateUrl);
+                        MobiFlightUpdaterModel.ManualUpgradeFromCommandLine(MobiFlightUpdaterModel.GetTheLastVersionNumberAvailable());
+                    }
+                    else
+                    {
+                        MessageBox.Show("You don't have right on this folder, Mobiflight can't install here !");
+                    }
                 }
-
-                return true;
-            }));
-
-            var result = await Task.WhenAny(tasks);
-            return result.Result;
+                Environment.Exit(0);
+            }
         }
-    }
+
+        private void OpenNotes(string Url)
+        {
+            ShowNotes.Navigate(new Uri(Url));
+        }
+
+        private void DownloadSelectedPackage()
+        {
+            ButtonInstall.Enabled = false;
+            StatusCurrent.Text = "Downloading ...";
+            progressBar1.Value = 0;
+            _webClient = new WebClient();
+            var uri = new Uri(_downloadURL);
+            CurrentFileName = MobiFlightUpdaterModel.GetFileName(_downloadURL);
+            _tempPath = Directory.GetCurrentDirectory() + "\\" + CurrentFileName;
+            if (!MobiFlightUpdaterModel.CheckIfFileIsHere(Directory.GetCurrentDirectory() + "\\" + CurrentFileName, _downloadChecksum)) //compare checksum if download the file is needeed
+            {
+                _webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(OnDownloadProgressChanged);
+                _webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(OnDownloadComplete);
+                _webClient.DownloadFileAsync(uri, _tempPath); // Download the file and extract in current directory
+            }
+            else // if zip file already here, installed it
+            {
+                Console.WriteLine("Installing package ...");
+                StatusCurrent.Text = "Installing package ...";
+                InstallPackage(CurrentFileName);
+            }
+        }
+
+        private void OnDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void OnDownloadComplete(object sender, AsyncCompletedEventArgs e)
+        {
+            if (!MobiFlightUpdaterModel.CheckIfFileIsHere(Directory.GetCurrentDirectory() + "\\" + CurrentFileName, _downloadChecksum)) //compare checksum if download is correct
+            {
+                Console.WriteLine("Download Completed ! installing package ...");
+                StatusCurrent.Text = "Download Completed ! installing package ...";
+                InstallPackage(CurrentFileName);
+                StatusCurrent.Text = "Package Installed !";
+            }
+            else
+            {
+                StatusCurrent.Text = "Download failed, checksum not match !";
+            }
+            _webClient.Dispose();
+        }
+
+        public void InstallPackage(string CurrentFileName)
+        {
+            if (Path.GetExtension(CurrentFileName) == ".zip")
+            {
+                MobiFlightUpdaterModel.CloseMobiflightAndWait();
+                MobiFlightUpdaterModel.GoExtractToDirectory(Directory.GetCurrentDirectory() + "\\" + CurrentFileName, Directory.GetCurrentDirectory());
+                Console.WriteLine("Package is installed !");
+                StatusCurrent.Text = "Package is installed !";
+                ActualInstalledVersion = MobiFlightUpdaterModel.GetActualInstalledVersion();
+                if (ActualInstalledVersion != "0.0.0")
+                {
+                    label1.Text = "MobiFlight " + ActualInstalledVersion + " is actually installed !";
+                    ButtonStartMF.Enabled = true;
+                }
+                else
+                {
+                    label1.Text = "Choose wich version do you want to install";
+                    ButtonStartMF.Enabled = false;
+                }
+            }
+            else if (Path.GetExtension(CurrentFileName) == ".exe")
+            {
+                MobiFlightUpdaterModel.StartProcessAndClose(CurrentFileName);
+            }
+            else
+            {
+                Console.WriteLine("Extention Package is not reconize");
+            }
+        }
+
+        private void PopulateListVersions()
+        {
+            ListVersions.Items.Clear();
+            foreach (KeyValuePair<string, Dictionary<string, string>> a in MobiFlightUpdaterModel.resultList)
+            {
+                if (a.Value["beta"] == "yes")
+                {
+                    ListVersions.Items.AddRange(new object[] { a.Key + " Beta" });
+                }
+                else
+                {
+                    ListVersions.Items.AddRange(new object[] { a.Key });
+                }
+            }
+            if (ListVersions.Items.Count > 0)
+                ListVersions.SetSelected(0, true);
+        }
+
+        private void ButtonExit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void ButtonDownload_Click(object sender, EventArgs e)
+        {
+            MobiFlightUpdaterModel.DownloadVersionsList(MobiFlightUpdateUrl);
+            PopulateListVersions();
+        }
+
+        private void ButtonInstall_Click(object sender, EventArgs e)
+        {
+            DownloadSelectedPackage();
+        }
+
+        private void ListVersions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] VersionSelectIndexArray = ListVersions.SelectedItem.ToString().Split(' ');
+            string VersionSelectIndex = VersionSelectIndexArray[0];
+            _downloadURL = MobiFlightUpdaterModel.resultList[VersionSelectIndex]["url"];
+            _downloadChecksum = MobiFlightUpdaterModel.resultList[VersionSelectIndex]["checksum"];
+            _downloadNotes = MobiFlightUpdaterModel.resultList[VersionSelectIndex]["changelog"];
+            ButtonInstall.Enabled = true;
+            OpenNotes(_downloadNotes);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            MobiFlightUpdaterModel.StartProcessAndClose(MobiFlightUpdaterModel.ProcessName);
+        }
+    }   
 }
