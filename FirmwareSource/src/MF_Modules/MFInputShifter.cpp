339c72ff8be9a1709b86698501b762a034f6927b
Neil Enns <info@danecreekphotography.com>
2021-08-30 20:20:14 -0700

    Reads the bits and (in theory) calls the registered handler

--- FirmwareSource/src/MF_Modules/MFInputShifter.cpp
+++ FirmwareSource/src/MF_Modules/MFInputShifter.cpp
@@ -4,13 +4,26 @@
 
 #include "MFInputShifter.h"
 
-MFInputShifter::MFInputShifter()
+void printBinary(unsigned long val)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    bool b = val & 0x80;
+    Serial.print(b);
+    val = val << 1;
+  }
+}
+
+MFInputShifter::MFInputShifter(const char *name)
 {
   _initialized = false;
   _lastState = 0;
-  _last = 0;
+  _last = millis();
+  _name = name;
 }
 
+// Registers a new input shifter and configures the clock, data and latch pins as well
+// as the number of modules to read from.
 void MFInputShifter::attach(uint8_t latchPin, uint8_t clockPin, uint8_t dataPin, uint8_t moduleCount)
 {
   _initialized = true;
@@ -24,32 +37,78 @@ void MFInputShifter::attach(uint8_t latchPin, uint8_t clockPin, uint8_t dataPin,
   pinMode(_dataPin, INPUT);
 }
 
+// Reads the values from the attached modules, compares them to the previously
+// read values, and calls the registered event handler for any inputs that
+// changed from the previously read state.
 void MFInputShifter::update()
 {
+  // Don't take any chances within the last 10 milliseconds to cover basic switch debouncing
   uint32_t now = millis();
   if (now - _last <= 10)
     return;
 
-  unsigned long currentState;
-  unsigned long changedBits;
+  uint8_t currentState;
 
-  digitalWrite(_clockPin, HIGH); // preset clock to retrieve first bit
-  digitalWrite(_latchPin, HIGH); // disable input latching and enable shifting
-  for (int i = 0; i < _moduleCount; i++)
-  {
-    currentState = shiftIn(_dataPin, _clockPin, MSBFIRST); // capture input values
-  }
-  digitalWrite(_latchPin, LOW); // disable shifting and enable input latching
+  digitalWrite(_clockPin, HIGH);                         // preset clock to retrieve first bit
+  digitalWrite(_latchPin, HIGH);                         // disable input latching and enable shifting
+  currentState = shiftIn(_dataPin, _clockPin, MSBFIRST); // capture input values
+  digitalWrite(_latchPin, LOW);                          // disable shifting and enable input latching
 
   if (currentState != _lastState)
   {
-    changedBits = currentState ^ _lastState; // Figure out which inputs changed
-    Serial.print("Changed bits: 0b");
-    Serial.println(changedBits, BIN);
+    Serial.print("Last state: ");
+    printBinary(_lastState);
+    Serial.print(" Current state: ");
+    printBinary(currentState);
+    Serial.println("");
+    detectChanges(_lastState, currentState);
     _lastState = currentState;
   }
 
   _last = now;
+}
+
+// Detects changes between the current state and the previously saved state
+// of the shifter inputs.
+void MFInputShifter::detectChanges(uint8_t lastState, uint8_t currentState)
+{
+  // TODO: Expand this so it handles the number of attached shifters
+  for (int i = 0; i < 8; i++)
+  {
+    // If last and current input state for the bit are different
+    // then the input changed and the handler for the bit needs to fire
+    if ((lastState & 1) ^ (currentState & 1))
+    {
+      trigger(i, currentState & 1);
+      Serial.print("Bit changed: ");
+      Serial.println(i);
+    }
+
+    lastState = lastState >> 1;
+    currentState = currentState >> 1;
+  }
+}
+
+// Triggers the event handler for the associated input shift register pin,
+// if a handler is registered.
+void MFInputShifter::trigger(uint8_t pin, bool state)
+{
+  if (state == LOW && _handlerList[pin][shifterOnPress] != NULL)
+  {
+    (*_handlerList[pin][shifterOnPress])(shifterOnPress, pin, _name);
+  }
+  else if (_handlerList[pin][shifterOnRelease] != NULL)
+  {
+    (*_handlerList[pin][shifterOnRelease])(shifterOnRelease, pin, _name);
+  }
+}
+
+// Attaches a new event handler for the specified pin. This is based on the
+// method from MFButton.cpp but handles multiple pins on a single input shift register.
+void MFInputShifter::attachHandler(uint8_t pin, byte eventId, inputShifterEvent newHandler)
+{
+  // TODO: Add bounds checking of the pin
+  _handlerList[pin][eventId] = newHandler;
 }
 
 void MFInputShifter::detach()
