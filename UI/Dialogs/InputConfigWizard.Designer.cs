9ad15ee98e7c3a5eca960edae195f5c38e77995d
Neil Enns <info@danecreekphotography.com>
2021-09-17 14:24:50 -0700

    Add pin dropdown to input config wizard

--- UI/Dialogs/InputConfigWizard.Designer.cs
+++ UI/Dialogs/InputConfigWizard.Designer.cs
@@ -477,6 +477,7 @@
             // 
             // inputTypeComboBox
             // 
+            this.inputTypeComboBox.DisplayMember = "Name";
             this.inputTypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.inputTypeComboBox.FormattingEnabled = true;
             this.inputTypeComboBox.Items.AddRange(new object[] {
@@ -485,7 +486,6 @@
             resources.GetString("inputTypeComboBox.Items2")});
             resources.ApplyResources(this.inputTypeComboBox, "inputTypeComboBox");
             this.inputTypeComboBox.Name = "inputTypeComboBox";
-            this.inputTypeComboBox.DisplayMember = "Name";
             this.inputTypeComboBox.SelectedIndexChanged += new System.EventHandler(this.inputTypeComboBox_SelectedIndexChanged);
             // 
             // displayTabTextBox
