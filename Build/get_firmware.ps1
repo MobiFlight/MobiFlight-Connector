f1a3cd5d4816ab417ddd0d698b84af554be7df61
Neil Enns <neile@live.com>
2021-10-28 11:48:32 -0700

    Add script to pull firmware releases from GitHub (#534)
    * Initial script to pull firmware on release build
    
    * Undo changes to project file
    
    * Change default output directory for script
    
    Co-authored-by: Neil Enns <{ID}+{username}@users.noreply.github.com>

---
+++ Build/get_firmware.ps1
@@ -0,0 +1,26 @@
+param ($version = "latest", $outDir = "../firmware")
+
+# Create the output folder if it doesn't exist
+if (!(Test-Path $outDir)) {
+  New-Item -ItemType Directory -Force -Path $outDir | Out-Null
+}
+
+if ($version -eq "latest") {
+  Write-Output "Downloading latest release"
+  $releaseDetails = Invoke-RestMethod -Uri "https://api.github.com/repos/MobiFlight/MobiFlight-FirmwareSource/releases/latest"
+}
+else {
+  Write-Output "Downloading firmware version $($version)"
+  $releaseDetails = Invoke-RestMethod -Uri "https://api.github.com/repos/MobiFlight/MobiFlight-FirmwareSource/releases/tags/v${version}"
+}
+
+# The list of assets also includes the zipped source code so filter it to just the
+# firmware files
+$firmwareFiles = $releaseDetails.assets | where-object { $_.name -match ".hex" }
+
+# Download all the firmware files to the correct location
+foreach ($firmware in $firmwareFiles) {
+  Write-Output "Downloading $($firmware.name)"
+  $outputFileName = Join-Path $outDir $firmware.name
+  Invoke-WebRequest $firmware.browser_download_url -OutFile $outputFileName
+}
