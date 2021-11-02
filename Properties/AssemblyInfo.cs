abff6bd7b4b1cd5c7682c973ae8bf779e8211a6c
Sebastian M <bitbucket@moebiuz.de>
2021-11-02 10:52:20 +0100

    Version 9.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.0.0.7")]
-[assembly: AssemblyFileVersion("9.0.0.7")]
+[assembly: AssemblyVersion("9.1.0")]
+[assembly: AssemblyFileVersion("9.1.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
