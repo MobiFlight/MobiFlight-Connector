3f73e0cd3731b099e82cff4dc209053cb03809e1
Neil Enns <{ID}+{username}@users.noreply.github.com>
2021-11-05 07:58:56 -0700

    Better version of the firmware update change

--- MobiFlight/MobiFlightFirmwareUpdater.cs
+++ MobiFlight/MobiFlightFirmwareUpdater.cs
@@ -31,7 +31,7 @@ namespace MobiFlight
             return File.Exists(filepath);
         }
 
-        public static bool UpdateViaAvrDude(MobiFlightModule module)
+        public static bool Update(MobiFlightModule module)
         {
             String Port = module.InitUploadAndReturnUploadPort();
             if (module.Connected) module.Disconnect();
@@ -41,12 +41,14 @@ namespace MobiFlight
                 System.Threading.Thread.Sleep(100);
             }
 
-            RunAvrDude(Port, module.Board);
-            
-            if (module.ArduinoType == MobiFlightModuleInfo.TYPE_ARDUINO_MICRO)
-                System.Threading.Thread.Sleep(1250);
+            if (module.Board.AvrDudeSettings != null)
+            {
+                RunAvrDude(Port, module.Board);
+                return true;
+            }
 
-            return true;
+            Log.Instance.log($"Firmware update requested for {module.Board.MobiFlightType} ({module.Port}) however no update settings were specified in the board definition file. Module update skipped.", LogSeverity.Warn);
+            return false;
         }
 
         /*
