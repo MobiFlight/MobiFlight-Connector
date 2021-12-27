01c327077d9ff33b68ceb619b26c3101bdd1acaa
Neil Enns <{ID}+{username}@users.noreply.github.com>
2021-12-27 13:24:40 -0800

    Rename command line parameter

--- Build/get_firmware.ps1
+++ Build/get_firmware.ps1
@@ -1,17 +1,17 @@
-param ($version = "latest", $outDir = "../firmware")
+param ($tag = "latest", $outDir = "../firmware")
 
 # Create the output folder if it doesn't exist
 if (!(Test-Path $outDir)) {
   New-Item -ItemType Directory -Force -Path $outDir | Out-Null
 }
 
-if ($version -eq "latest") {
+if ($tag -eq "latest") {
   Write-Output "Downloading latest release"
   $releaseDetails = Invoke-RestMethod -Uri "https://api.github.com/repos/MobiFlight/MobiFlight-FirmwareSource/releases/latest"
 }
 else {
-  Write-Output "Downloading firmware version $($version)"
-  $releaseDetails = Invoke-RestMethod -Uri "https://api.github.com/repos/MobiFlight/MobiFlight-FirmwareSource/releases/tags/${version}"
+  Write-Output "Downloading firmware tag $($tag)"
+  $releaseDetails = Invoke-RestMethod -Uri "https://api.github.com/repos/MobiFlight/MobiFlight-FirmwareSource/releases/tags/${tag}"
 }
 
 # The list of assets also includes the zipped source code so filter it to just the
