4082999c2974ffe7f4ca3f320b306378fd9bf4aa
GioCC <25667790+GioCC@users.noreply.github.com>
2022-01-06 00:12:29 +0100

    Fix for #643-Incorrect update of pin selection combo boxes TEST VERSION, LIMITED TO LedSegment!

--- Base/ComboBoxHelper.cs
+++ Base/ComboBoxHelper.cs
@@ -47,7 +47,13 @@ namespace System
 
         static public bool BindMobiFlightFreePins(ComboBox comboBox, List<MobiFlightPin> Pins, String CurrentPin, bool analogOnly = false)
         {
+            // This function returns a list of all currently free pins, plus the specified one marked as free.
+            // Required because, in a selection list for a device signal, the already assigned pin must be in the list in order to be selectable.
+            
+            if (Pins == null) return false;
+            // Deep-clone list as 'Used' list
             List<MobiFlightPin> UsablePins = Pins.ConvertAll(pin => new MobiFlightPin(pin));
+            // Mark current pin as free
             if (UsablePins.Exists(x => x.Pin == byte.Parse(CurrentPin)))
                 UsablePins.Find(x => x.Pin == byte.Parse(CurrentPin)).Used = false;
 
@@ -56,11 +62,30 @@ namespace System
                 UsablePins = UsablePins.FindAll(x => x.isAnalog == true);
             }
 
+            // Assign the all-free list to the combobox
             comboBox.DataSource = UsablePins.FindAll(x => x.Used == false);
             comboBox.DisplayMember = "Name";
             comboBox.ValueMember = "Pin";
 
             return false;
+        }
+        static public void reassignPin(ComboBox comboBox, List<MobiFlightPin> pinList, ref string before)
+        {
+            string after = comboBox.SelectedItem.ToString();
+            byte nBefore = byte.Parse(before);
+            byte nAfter = byte.Parse(after);
+            try {
+                if (before != after) {
+                    pinList.Find(x => x.Pin == nBefore).Used = false;
+                    pinList.Find(x => x.Pin == nAfter).Used = true;
+                }
+            }
+            catch (Exception e) {
+                Log.Instance.log($"Pin reassignment from {before} to {after} went wrong", LogSeverity.Debug);
+            }
+            ComboBoxHelper.BindMobiFlightFreePins(comboBox, pinList, after);
+            comboBox.SelectedValue = nAfter;
+            before = after;
         }
     }
 }
