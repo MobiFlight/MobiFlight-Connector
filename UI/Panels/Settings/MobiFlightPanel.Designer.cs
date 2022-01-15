b8af54eaae9b194daa5d3afa17fa1669dee7b4fd
Sebastian M <bitbucket@moebiuz.de>
2022-01-15 13:17:26 +0100

    MobiFlightPanel: Reverted wrong icon size for toolbar

--- UI/Panels/Settings/MobiFlightPanel.Designer.cs
+++ UI/Panels/Settings/MobiFlightPanel.Designer.cs
@@ -112,7 +112,6 @@
             // 
             // mfModuleSettingsContextMenuStrip
             // 
-            this.mfModuleSettingsContextMenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.mfModuleSettingsContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.addToolStripMenuItem,
             this.removeToolStripMenuItem,
@@ -281,7 +280,6 @@
             // mobiflightSettingsToolStrip
             // 
             resources.ApplyResources(this.mobiflightSettingsToolStrip, "mobiflightSettingsToolStrip");
-            this.mobiflightSettingsToolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.mobiflightSettingsToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.uploadToolStripButton,
             this.toolStripSeparator1,
