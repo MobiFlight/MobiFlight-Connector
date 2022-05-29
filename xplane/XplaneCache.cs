5ae58a460485f516dd9bd71be024e4d3b985dbd0
Sebastian M <bitbucket@moebiuz.de>
2022-05-29 20:27:04 +0200

    Updated Target Framework and all NuGet Packages

---
+++ xplane/XplaneCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiFlight.xplane
+{
+    internal class XplaneCache
+    {
+        public event EventHandler Closed;
+        public event EventHandler Connected;
+        public event EventHandler ConnectionLost;
+
+        private bool _simConnectConnected = false;
+        private bool _connected = false;
+
+        public bool Connect()
+        {
+            return false;
+        }
+        public bool Disconnect()
+        {
+            return false;
+        }
+
+        public int readDataRef(string dataRefPath)
+        {
+            return 0;
+        }
+
+        public void writeDataRef(string dataRefPath, int value)
+        {
+
+        }
+
+        public void sendCommand(string commmand)
+        {
+
+        }
+    }
+}
