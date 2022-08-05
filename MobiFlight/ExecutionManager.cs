MobiFlight/MobiFlight-Connector

commit 60cadb0fdf984c816733decc5ce07cb9fd63afaa

Connection to sim established in Offline Mode (#873)

MobiFlight/ExecutionManager.cs
@@ -74,7 +74,6 @@ namespace MobiFlight
 #if ARCAZE
         readonly ArcazeCache arcazeCache = new ArcazeCache();
 #endif
-        public bool OfflineMode { get; set; }
 
 #if MOBIFLIGHT
         readonly MobiFlightCache mobiFlightCache = new MobiFlightCache();
@@ -280,19 +279,6 @@ namespace MobiFlight
             Log.Instance.log("ExecutionManager.AutoConnectStart:" + "Started auto connect timer", LogSeverity.Debug);
         }
 
-        public void ReconnectSim()
-        {
-            fsuipcCache.Disconnect();
-            fsuipcCache.Connect();
-#if SIMCONNECT
-            simConnectCache.Disconnect();
-            simConnectCache.Connect();
-#endif
-            xplaneCache.Disconnect();
-            xplaneCache.Connect();
-
-        }
-
         public void AutoConnectStop()
         {
             Log.Instance.log("ExecutionManager.AutoConnectStop:" + "Stopped auto connect timer", LogSeverity.Debug);
@@ -456,24 +442,19 @@ namespace MobiFlight
                 if (cfg.SourceType == SourceType.FSUIPC && !fsuipcCache.IsConnected())
                 {
                     row.ErrorText = i18n._tr("uiMessageNoFSUIPCConnection");
-                    if (!OfflineMode) continue;
                 }
 #if SIMCONNECT
                 // If not connected to SimConnect show an error message
                 if (cfg.SourceType == SourceType.SIMCONNECT && !simConnectCache.IsConnected())
                 {
                     row.ErrorText = i18n._tr("uiMessageNoSimConnectConnection");
-                    if (!OfflineMode) continue;
                 }
 #endif
-
-                // If not connected to SimConnect show an error message
+                // If not connected to X-Plane show an error message
                 if (cfg.SourceType == SourceType.XPLANE && !xplaneCache.IsConnected())
                 {
                     row.ErrorText = i18n._tr("uiMessageNoSimConnectConnection");
-                    if (!OfflineMode) continue;
                 }
-                // if (cfg.FSUIPCOffset == ArcazeConfigItem.FSUIPCOffsetNull) continue;
 
                 ConnectorValue value = ExecuteRead(cfg);
                 ConnectorValue processedValue = value;
@@ -1286,13 +1267,7 @@ namespace MobiFlight
         {
             if (_autoConnectTimerRunning) return;
             _autoConnectTimerRunning = true;
-            // check if timer is running... 
-            // do nothing if so, since everything else has been checked before...            
-            if (timer.Enabled || testModeTimer.Enabled)
-            {
-                _autoConnectTimerRunning = false;
-                return;
-            }
+
 
             if (
 #if ARCAZE
@@ -1314,7 +1289,7 @@ namespace MobiFlight
             }
 
             // Check only for available sims if not in Offline mode.
-            if (!OfflineMode) { 
+            if (true) { 
 
                 if (SimAvailable())
                 {
@@ -1348,10 +1323,6 @@ namespace MobiFlight
                 }
             }
 
-            // this line here provokes a timer stop event each time
-            // and therefore the icon for starting the app will get enabled
-            // @see timer_Stopped
-            timer.Enabled = false;
             _autoConnectTimerRunning = false;
         } //autoConnectTimer_Tick()
 

MobiFlight/FlightSim.cs
@@ -33,7 +33,7 @@ namespace MobiFlight
     {
         static public FlightSimConnectionMethod FlightSimConnectionMethod = FlightSimConnectionMethod.NONE;
         static public FlightSimType FlightSimType = FlightSimType.NONE;
-        static public bool OfflineMode = false;
+        
         static public Dictionary<FlightSimConnectionMethod, String> SimConnectionNames = new Dictionary<FlightSimConnectionMethod, string>()
         {
             { FlightSimConnectionMethod.NONE, "None" },
@@ -124,13 +124,6 @@ namespace MobiFlight
             {
                 FlightSimConnectionMethod = FlightSimConnectionMethod.WIDECLIENT;
                 FlightSimType = FlightSimType.XPLANE;
-                return true;
-            }
-
-            if (OfflineMode)
-            {
-                FlightSimConnectionMethod = FlightSimConnectionMethod.OFFLINE;
-                FlightSimType = FlightSimType.UNKNOWN;
                 return true;
             }
 

SimConnectMSFS/SimConnectCache.cs
@@ -296,11 +296,14 @@ namespace MobiFlight.SimConnectMSFS
                 m_oSimConnect = null;
             }
 
-            _simConnectConnected = false;
-            _connected = false;
+            if (_simConnectConnected || _connected)
+            {
+                _simConnectConnected = false;
+                _connected = false;
 
-            Closed?.Invoke(this, null);
-            
+                Closed?.Invoke(this, null);
+            }
+
             return true;
         }
 
@@ -343,6 +346,9 @@ namespace MobiFlight.SimConnectMSFS
         public float GetSimVar(String SimVarName)
         {
             float result = 0;
+            if (!IsConnected()) 
+                return result;
+
             if (!SimVars.Exists(lvar => lvar.Name == SimVarName))
             {
                 RegisterSimVar(SimVarName);

UI/MainForm.cs
@@ -157,9 +157,6 @@ namespace MobiFlight.UI
 
             execManager.getMobiFlightModuleCache().ModuleConnecting += MainForm_ModuleConnected;
 
-            execManager.OfflineMode = Properties.Settings.Default.OfflineMode;
-
-            if (execManager.OfflineMode) OfflineModeIconToolStripStatusLabel.Image = Properties.Resources.lightbulb_on;
             FsuipcToolStripMenuItem.Image = Properties.Resources.warning;
             simConnectToolStripMenuItem.Image = Properties.Resources.warning;
 
@@ -506,14 +503,6 @@ namespace MobiFlight.UI
                 execManager.SetPollInterval((int)e.NewValue);
             }
 
-            if (e.SettingName == "OfflineMode")
-            {
-                execManager.OfflineMode = (bool)e.NewValue;
-                execManager.ReconnectSim();
-                if (execManager.OfflineMode) OfflineModeIconToolStripStatusLabel.Image = Properties.Resources.lightbulb_on;
-                else OfflineModeIconToolStripStatusLabel.Image = Properties.Resources.lightbulb;
-            }
-
             if (e.SettingName == "CommunityFeedback")
             {
                 AppTelemetry.Instance.Enabled = Properties.Settings.Default.CommunityFeedback;
@@ -632,12 +621,8 @@ namespace MobiFlight.UI
         private bool RunIsAvailable()
         {
             return 
-                   // Offline Mode Or Sim available
-                   (execManager.OfflineMode || execManager.SimConnected()) &&
-                   // Hardware available
-                   (execManager.ModulesConnected() || execManager.GetJoystickManager().JoysticksConnected()) && 
-                   // We are not already running
-                   !execManager.IsStarted() && !execManager.TestModeIsStarted();
+                // We are not already running
+                !execManager.IsStarted() && !execManager.TestModeIsStarted();
         }
 
         /// <summary>

UI/Panels/Settings/GeneralPanel.Designer.cs
@@ -35,9 +35,6 @@
             this.label9 = new System.Windows.Forms.Label();
             this.languageComboBox = new System.Windows.Forms.ComboBox();
             this.languageLabel = new System.Windows.Forms.Label();
-            this.offlineModeGroupBox = new System.Windows.Forms.GroupBox();
-            this.OfflineModeLabel = new System.Windows.Forms.Label();
-            this.offlineModeCheckBox = new System.Windows.Forms.CheckBox();
             this.debugGroupBox = new System.Windows.Forms.GroupBox();
             this.LogJoystickAxisCheckBox = new System.Windows.Forms.CheckBox();
             this.logLevelComboBox = new System.Windows.Forms.ComboBox();
@@ -60,7 +57,6 @@
             this.SpeedTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.BetaUpdatesGroupBox.SuspendLayout();
             this.languageGroupBox.SuspendLayout();
-            this.offlineModeGroupBox.SuspendLayout();
             this.debugGroupBox.SuspendLayout();
             this.testModeSpeedGroupBox.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.testModeSpeedTrackBar)).BeginInit();
@@ -113,25 +109,6 @@
             // 
             resources.ApplyResources(this.languageLabel, "languageLabel");
             this.languageLabel.Name = "languageLabel";
-            // 
-            // offlineModeGroupBox
-            // 
-            this.offlineModeGroupBox.Controls.Add(this.OfflineModeLabel);
-            this.offlineModeGroupBox.Controls.Add(this.offlineModeCheckBox);
-            resources.ApplyResources(this.offlineModeGroupBox, "offlineModeGroupBox");
-            this.offlineModeGroupBox.Name = "offlineModeGroupBox";
-            this.offlineModeGroupBox.TabStop = false;
-            // 
-            // OfflineModeLabel
-            // 
-            resources.ApplyResources(this.OfflineModeLabel, "OfflineModeLabel");
-            this.OfflineModeLabel.Name = "OfflineModeLabel";
-            // 
-            // offlineModeCheckBox
-            // 
-            resources.ApplyResources(this.offlineModeCheckBox, "offlineModeCheckBox");
-            this.offlineModeCheckBox.Name = "offlineModeCheckBox";
-            this.offlineModeCheckBox.UseVisualStyleBackColor = true;
             // 
             // debugGroupBox
             // 
@@ -277,15 +254,12 @@
             this.Controls.Add(this.languageGroupBox);
             this.Controls.Add(this.CommunityFeedbackGroupBox);
             this.Controls.Add(this.BetaUpdatesGroupBox);
-            this.Controls.Add(this.offlineModeGroupBox);
             this.Controls.Add(this.debugGroupBox);
             this.Controls.Add(this.recentFilesGroupBox);
             this.Name = "GeneralPanel";
             this.BetaUpdatesGroupBox.ResumeLayout(false);
             this.BetaUpdatesGroupBox.PerformLayout();
             this.languageGroupBox.ResumeLayout(false);
-            this.offlineModeGroupBox.ResumeLayout(false);
-            this.offlineModeGroupBox.PerformLayout();
             this.debugGroupBox.ResumeLayout(false);
             this.debugGroupBox.PerformLayout();
             this.testModeSpeedGroupBox.ResumeLayout(false);
@@ -310,9 +284,6 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.ComboBox languageComboBox;
         private System.Windows.Forms.Label languageLabel;
-        private System.Windows.Forms.GroupBox offlineModeGroupBox;
-        private System.Windows.Forms.Label OfflineModeLabel;
-        private System.Windows.Forms.CheckBox offlineModeCheckBox;
         private System.Windows.Forms.GroupBox debugGroupBox;
         private System.Windows.Forms.ComboBox logLevelComboBox;
         private System.Windows.Forms.Label logLevelLabel;

UI/Panels/Settings/GeneralPanel.cs
@@ -55,9 +55,6 @@ namespace MobiFlight.UI.Panels.Settings
             ComboBoxHelper.SetSelectedItem(logLevelComboBox, Properties.Settings.Default.LogLevel);
             LogJoystickAxisCheckBox.Checked = Properties.Settings.Default.LogJoystickAxis;
 
-            // Offline Mode
-            offlineModeCheckBox.Checked = Properties.Settings.Default.OfflineMode;
-
             // System Language
             languageComboBox.SelectedValue = Properties.Settings.Default.Language;
 
@@ -90,9 +87,6 @@ namespace MobiFlight.UI.Panels.Settings
 
             Properties.Settings.Default.LogJoystickAxis = LogJoystickAxisCheckBox.Checked;
             Log.Instance.LogJoystickAxis = LogJoystickAxisCheckBox.Checked;
-
-            // Offline Mode
-            Properties.Settings.Default.OfflineMode = offlineModeCheckBox.Checked;
 
             // System Language
             Properties.Settings.Default.Language = languageComboBox.SelectedValue.ToString();

commit 31b01ba460883fa65fa6d78a3e6e10430d3e9289

Fixed the issue that got introduced with #873

MobiFlight/ExecutionManager.cs
@@ -499,7 +499,8 @@ namespace MobiFlight
                 }
                 else
                 {
-                    row.ErrorText = "";
+                    if (row.ErrorText == i18n._tr("uiMessagePreconditionNotSatisfied"))
+                        row.ErrorText = "";
                 }
 
                 try