ae8db74fce2dac88f4ab9e13c6ec174eaae96b31
Sebastian M <sebastian@mobiflight.com>
2022-10-15 23:12:40 +0200

    Fixes problem with LCD lines longer than display

---
+++ Base/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiFlight.Base
+{
+    public static class Extensions
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+    }
+}
