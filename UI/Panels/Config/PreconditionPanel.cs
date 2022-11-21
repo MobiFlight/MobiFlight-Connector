3677e167572dfa2a215e19247ac094c792991432
Sebastian M <sebastian@mobiflight.com>
2022-11-22 00:33:10 +0100

    removing unnecessary imports

--- UI/Panels/Config/PreconditionPanel.cs
+++ UI/Panels/Config/PreconditionPanel.cs
@@ -3,10 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MobiFlight.UI.Panels.Config
