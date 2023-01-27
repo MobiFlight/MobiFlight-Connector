3e6c213f7c2b363180587435543fee3d1bbeac26
Sebastian M <sebastian@mobiflight.com>
2023-01-27 18:42:17 +0100

    Provide correct error label when xplane connection is missing

--- UI/Panels/OutputConfigPanel.cs
+++ UI/Panels/OutputConfigPanel.cs
@@ -1,14 +1,9 @@
-﻿using System;
+﻿using MobiFlight.UI.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MobiFlight.UI.Dialogs;
-using MobiFlight.Base;
 
 namespace MobiFlight.UI.Panels
 {
