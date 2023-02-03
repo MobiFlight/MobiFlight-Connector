59229bcb13323dc835839b5e455923e5c2e4603e
Sebastian M <sebastian@mobiflight.com>
2023-02-03 16:40:03 +0100

    Linebreaks are correctly displayed in code field (#1112)

--- Base/Extensions.cs
+++ Base/Extensions.cs
@@ -13,5 +13,21 @@ namespace MobiFlight.Base
             if (string.IsNullOrEmpty(value)) return value;
             return value.Length <= maxLength ? value : value.Substring(0, maxLength);
         }
+
+        public static string ToCRLF(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            // handle mixed line endings correctly (avoid expansion of \n in \r\n -> \r\n\n)
+            // first, convert all line-endings to only LF
+            // then, convert all line-endings to CRLF
+            return value.Replace("\r\n", "\n").Replace("\n", "\r\n");
+        }
+
+        public static string ToLF(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+            // mixed lines are treated correctly
+            return value.Replace("\r\n", "\n");
+        }
     }
 }
