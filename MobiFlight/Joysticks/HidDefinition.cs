1b9919ccc5d4f4cda29050cd7098289338ba00a6
Neil Enns <{ID}+{username}@users.noreply.github.com>
2023-02-05 07:12:39 -0800

    Initial pass at reading from json files

---
+++ MobiFlight/Joysticks/HidDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobiFlight
+{
+    public class HidDefinition
+    {
+        public string InstanceName;
+        public List<HidInput> Inputs;
+        public List<HidOutput> Outputs;
+
+        public void Migrate() { }
+    }
+}
