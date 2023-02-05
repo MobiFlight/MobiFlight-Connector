3860272d30e5b9afa331c1057b6d4c58cf14ea71
Neil Enns <{ID}+{username}@users.noreply.github.com>
2023-02-05 07:17:26 -0800

    Handle empty definition object, clean up code a bit

--- MobiFlight/Joysticks/SaitekAviatorStick.cs
+++ MobiFlight/Joysticks/SaitekAviatorStick.cs
@@ -6,14 +6,13 @@ namespace MobiFlight.Joysticks
     {
         int VendorId = 0x06A3;
         int ProductId = 0x0461;
-
-        HidDefinition Definition;
+        readonly HidDefinition Definition;
 
         public SaitekAviatorStick(SharpDX.DirectInput.Joystick joystick, HidDefinition definition) : base(joystick) {
             Definition = definition;
             Labels = new Dictionary<string, string>();
 
-            definition.Inputs.ForEach(input => Labels.Add(input.Name, input.Label));
+            Definition?.Inputs.ForEach(input => Labels.Add(input.Name, input.Label));
         }
     }
 }
