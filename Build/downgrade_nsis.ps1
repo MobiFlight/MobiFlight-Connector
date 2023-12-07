7579acb257b520c13132ceb3b668127e573b8d2c
Neil Enns <neile@live.com>
2023-12-06 23:25:18 -0800

    Change download method for NSIS (#1477)
    Fixes #1476

--- Build/downgrade_nsis.ps1
+++ Build/downgrade_nsis.ps1
@@ -4,8 +4,14 @@
 ################################################################################
 
 $NsisVersion = "3.04"
-Install-Binary -Url "https://downloads.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -Name  "nsis-${NsisVersion}-setup.exe" -ArgumentList ('/S')
+Invoke-WebRequest "https://netcologne.dl.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -OutFile "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe"
+Start-Process -Wait -FilePath "C:\WINDOWS\Temp\nsis-${NsisVersion}-setup.exe" -ArgumentList "/S"
 
+# Add the newly installed version to the path.
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
 Add-MachinePathItem $NsisPath
 $env:Path = Get-MachinePath
+
+# Write out the version that's now on the path for confirmation of the installed version
+# in GitHub action logs.
+makensis.exe /VERSION
