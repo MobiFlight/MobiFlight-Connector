fca31b4bf1e8950abb1b5a8f87c4ed589c338742
Sebastian M <sebastian@mobiflight.com>
2023-12-13 23:45:38 +0100

    make it a bit shorter.

--- Build/downgrade_nsis.ps1
+++ Build/downgrade_nsis.ps1
@@ -2,44 +2,13 @@
 ##  File:  Install-NSIS.ps1
 ##  Desc:  Install NSIS
 ################################################################################
-function Set-MachinePath{
-    [CmdletBinding()]
-    param(
-        [string]$NewPath
-    )
-    Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name Path -Value $NewPath
-    return $NewPath
-}
-
-function Add-MachinePathItem
-{
-    [CmdletBinding()]
-    param(
-        [string]$PathItem
-    )
-
-    $currentPath = Get-MachinePath
-    $newPath = $PathItem + ';' + $currentPath
-    return Set-MachinePath -NewPath $newPath
-}
-
-function Get-MachinePath{
-    [CmdletBinding()]
-    param(
-
-    )
-    $currentPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-    return $currentPath
-}
-
 $NsisVersion = "3.04"
 # Get-Help Install-Binary -Full
 
 Install-Binary -Url "https://downloads.sourceforge.net/project/nsis/NSIS%203/${NsisVersion}/nsis-${NsisVersion}-setup.exe" -Type EXE -InstallArgs ('/S')
 
 $NsisPath = "${env:ProgramFiles(x86)}\NSIS\"
-Add-MachinePathItem $NsisPath
-$env:Path = Get-MachinePath
+$env:PATH += ";$NsisPath"
 
 # Write out the version that's now on the path for confirmation of the installed version
 # in GitHub action logs.
