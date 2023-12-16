6b4b346c32759ba89514b89cf7d7ddc070ab02a3
Koseng <26673978+Koseng@users.noreply.github.com>
2023-12-16 10:14:38 +0100

    Add async await

--- MobiFlight/Monitors/DeviceMonitor.cs
+++ MobiFlight/Monitors/DeviceMonitor.cs
@@ -29,13 +29,13 @@ namespace MobiFlight.Monitors
             Scan();
         }
 
-        public void Start()
+        public async void Start()
         {
             EventWatcher.EventArrived += EventWatcher_EventArrived;
             EventWatcher.Start();
 
             // Currently thread change necessary, otherwise exception in board reset process.
-            Task.Delay(50).ContinueWith(_ => Scan());        
+            await Task.Delay(50).ContinueWith(_ => Scan());                
         }
 
         public void Stop()
