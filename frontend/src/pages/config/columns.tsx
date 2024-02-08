bd1daa4605535af75241849f7f375f27eb3d1898
Sebastian M <sebastian@mobiflight.com>
2024-02-08 19:59:04 +0100

    Added context menu

--- frontend/src/pages/config/columns.tsx
+++ frontend/src/pages/config/columns.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu"
 import { Switch } from "@/components/ui/switch"
 import { IConfigItem } from "@/types"
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
 import { ColumnDef } from "@tanstack/react-table"
+import { IconDots } from "@tabler/icons-react"
 
 export const columns: ColumnDef<IConfigItem>[] = [
     {
@@ -8,7 +12,7 @@ export const columns: ColumnDef<IConfigItem>[] = [
         header: () => <div className="text-slate-900">Active</div>,
         cell: ({ row }) => {
             return <Switch checked={row.getValue("Active") as boolean} />
-          },
+        },
     },
     {
         accessorKey: "Description",
@@ -41,5 +45,32 @@ export const columns: ColumnDef<IConfigItem>[] = [
     {
         accessorKey: "FinalValue",
         header: "Final Value",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const payment = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <IconDots className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Duplicate</DropdownMenuItem>
+                        <DropdownMenuItem>Copy</DropdownMenuItem>
+                        <DropdownMenuItem>Paste</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Test</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
     }
 ]
