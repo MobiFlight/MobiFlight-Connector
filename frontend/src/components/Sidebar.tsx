3c1b3b93ee4d53c96aa94266d95a7cadcd368970
Sebastian M <sebastian@mobiflight.com>
2024-02-10 12:26:38 +0100

    Added highlight border effect in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,8 @@
-import { Link, useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 import { DarkModeToggle } from './mobiflight/darkmode-toggle'
 import { IconDeviceGamepad2, IconList, IconSettings } from '@tabler/icons-react';
 import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
 
 type SidebarProps = {
   className?: string
@@ -9,30 +10,31 @@ type SidebarProps = {
 
 const Sidebar = (props: SidebarProps) => {
   const params = useParams()
+  const activeClassNameProps = "dark:bg-gray-700"
 
 
   return (
     <div className={`${props.className} bg-primary dark:bg-slate-800 w-16 flex flex-col justify-between items-center pb-8 z-50`}>
       <div className='flex flex-col items-center gap-8 pt-24'>
-        <Link to={`/projects/${params.id}/configs`}>
-          <Button variant="outline" className='w-12 h-12' size="icon">
+        <NavLink to={`/projects/${params.id}/configs`}>
+          <Button variant="outline" className={cn(activeClassNameProps, `hover:border hover:border-gray-500 w-12 h-12`)} size="icon">
             <IconList></IconList>
           </Button>
-        </Link>
-        <Link to={`/devices`}>
-          <Button variant="outline" className='w-12 h-12' size="icon">
+        </NavLink>
+        <NavLink to={`/devices`}>
+          <Button variant="outline" className={cn(activeClassNameProps, `hover:border hover:border-gray-500 w-12 h-12`)} size="icon">
             <IconDeviceGamepad2></IconDeviceGamepad2>
           </Button>
-        </Link>
+        </NavLink>
 
       </div>
       <div className='flex flex-col items-center gap-8'>
-        <Link to="/settings">
-          <Button variant="outline" className='w-12 h-12' size="icon">
+        <NavLink to="/settings">
+          <Button variant="outline" className={cn(activeClassNameProps, `hover:border hover:border-gray-500 w-12 h-12`)} size="icon">
             <IconSettings></IconSettings>
           </Button>
-        </Link>
-        <DarkModeToggle></DarkModeToggle>
+        </NavLink>
+        <DarkModeToggle className="hover:border hover:border-gray-500"></DarkModeToggle>
       </div>
     </div>
   )
--- frontend/src/components/mobiflight/darkmode-toggle.tsx
+++ frontend/src/components/mobiflight/darkmode-toggle.tsx
@@ -8,14 +8,19 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useTheme } from "@/components/ui/theme-provider"
+import { cn } from "@/lib/utils"
 
-export function DarkModeToggle() {
+interface DarkModeToggleProps {
+  className?: string
+}
+
+export function DarkModeToggle(props: DarkModeToggleProps) {
   const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="w-12 h-12 p-0">
+        <Button variant="outline" className={cn(props.className,`w-12 h-12 p-0`)}>
           <IconSunHigh className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 stroke-14" />
           <IconMoon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
