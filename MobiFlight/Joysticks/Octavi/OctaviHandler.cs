b1006435ce6f95f1f25dd0d8fe5613c9dee06d91
Felix von Plehwe <felixvp@gmail.com>
2024-02-26 15:41:54 +0100

    Octavi shift modes controlled by .json file-Reviewed_2

--- MobiFlight/Joysticks/Octavi/OctaviHandler.cs
+++ MobiFlight/Joysticks/Octavi/OctaviHandler.cs
@@ -1,8 +1,6 @@
-﻿using Antlr.Runtime.Tree;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
 namespace MobiFlight.Joysticks.Octavi
 {
