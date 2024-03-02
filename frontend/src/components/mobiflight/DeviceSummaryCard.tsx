dea2feccabf3b32d8ffa39791c915a8ea8be35d1
Sebastian M <sebastian@mobiflight.com>
2024-03-02 19:54:48 +0100

    Show additional device options only for MobiFlight boards

--- frontend/src/components/mobiflight/DeviceSummaryCard.tsx
+++ frontend/src/components/mobiflight/DeviceSummaryCard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
-import { IconDots } from "@tabler/icons-react";
-import { IDeviceItem } from "@/types";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card"
+import { IconDots } from "@tabler/icons-react"
+import { IDeviceItem } from "@/types"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,34 +8,38 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
-import { publishOnMessageExchange } from "@/lib/hooks";
-import { cn } from "@/lib/utils";
-import { NavLink } from "react-router-dom";
-import ConditionalWrapper from "./ConditionalWrapper";
-import { Progress } from "@/components/ui/progress";
-import { Switch } from "@/components/ui/switch";
+} from "../ui/dropdown-menu"
+import { publishOnMessageExchange } from "@/lib/hooks"
+import { cn } from "@/lib/utils"
+import { NavLink } from "react-router-dom"
+import ConditionalWrapper from "./ConditionalWrapper"
+import { Progress } from "@/components/ui/progress"
+import { Switch } from "@/components/ui/switch"
 
 export type DeviceSummaryCardProps = {
-  device: IDeviceItem;
-  className?: string;
-  navLink?: string;
-};
+  device: IDeviceItem
+  className?: string
+  navLink?: string
+}
 
 const DeviceSummaryCard = (props: DeviceSummaryCardProps) => {
-  const { device, className, navLink } = props;
-  const { publish } = publishOnMessageExchange();
-  const usedPins = device.Pins ? device.Pins?.filter((pin) => pin.Used).length : 0;
-  const usedPercentage = device.Pins ? Math.round(usedPins * 100 / device.Pins!.length) : 0
+  const { device, className, navLink } = props
+  const { publish } = publishOnMessageExchange()
+  const usedPins = device.Pins
+    ? device.Pins?.filter((pin) => pin.Used).length
+    : 0
+  const usedPercentage = device.Pins
+    ? Math.round((usedPins * 100) / device.Pins!.length)
+    : 0
   console.log(usedPins, usedPercentage)
 
   const uploadConfigRequest = () => {
-    console.log("Upload config request");
+    console.log("Upload config request")
     publish({
       key: "DeviceUploadMessage",
       payload: device,
-    });
-  };
+    })
+  }
 
   const openFromFileRequest = () => {
     publish({
@@ -44,8 +48,8 @@ const DeviceSummaryCard = (props: DeviceSummaryCardProps) => {
         IgnoreTypeMismatch: false,
         Device: device,
       },
-    });
-  };
+    })
+  }
 
   const saveToFileRequest = () => {
     publish({
@@ -53,22 +57,22 @@ const DeviceSummaryCard = (props: DeviceSummaryCardProps) => {
       payload: {
         Device: device,
       },
-    });
-  };
+    })
+  }
 
   const firmwareUpdateRequest = () => {
     publish({
       key: "DeviceFirmwareUpdateRequest",
       payload: device,
-    });
-  };
+    })
+  }
 
   const firmwareResetRequest = () => {
     publish({
       key: "DeviceFirmwareResetRequest",
       payload: device,
-    });
-  };
+    })
+  }
 
   return (
     <Card className={className}>
@@ -106,54 +110,55 @@ const DeviceSummaryCard = (props: DeviceSummaryCardProps) => {
           )}
         </CardContent>
       </ConditionalWrapper>
-      <CardFooter className="flex flex-row justify-between gap-4 group">
+      <CardFooter className="group flex flex-row justify-between gap-4">
         <Switch>Active</Switch>
         {device.Pins && (
           <Progress
             color={cn(
-              "bg-gray-200 group-hover/card:bg-green-800", 
+              "bg-gray-200 group-hover/card:bg-green-800",
               usedPercentage > 60 ? "group-hover/card:bg-green-600" : "",
               usedPercentage > 70 ? "group-hover/card:bg-blue-500" : "",
               usedPercentage > 90 ? "group-hover/card:bg-primary" : "",
-              ) 
-            }
+            )}
             className={"w-1/2"}
             value={usedPercentage}
           />
         )}
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <IconDots className="cursor-pointer"></IconDots>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent side="top" align="end">
-            <DropdownMenuItem onClick={uploadConfigRequest}>
-              Upload config
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel>Backup</DropdownMenuLabel>
-            <DropdownMenuItem onClick={openFromFileRequest}>
-              Open from file
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={saveToFileRequest}>
-              Save to file
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel>Firmware</DropdownMenuLabel>
-            <DropdownMenuItem onClick={firmwareUpdateRequest}>
-              Update
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={firmwareResetRequest}>
-              Reset
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuLabel>Troubleshooting</DropdownMenuLabel>
-            <DropdownMenuItem>Regenerate serial</DropdownMenuItem>
-            <DropdownMenuItem>Reload config</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {device.Type === "MobiFlight" && (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <IconDots className="cursor-pointer"></IconDots>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="top" align="end">
+              <DropdownMenuItem onClick={uploadConfigRequest}>
+                Upload config
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuLabel>Backup</DropdownMenuLabel>
+              <DropdownMenuItem onClick={openFromFileRequest}>
+                Open from file
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={saveToFileRequest}>
+                Save to file
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuLabel>Firmware</DropdownMenuLabel>
+              <DropdownMenuItem onClick={firmwareUpdateRequest}>
+                Update
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={firmwareResetRequest}>
+                Reset
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuLabel>Troubleshooting</DropdownMenuLabel>
+              <DropdownMenuItem>Regenerate serial</DropdownMenuItem>
+              <DropdownMenuItem>Reload config</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </CardFooter>
     </Card>
-  );
-};
+  )
+}
 
-export default DeviceSummaryCard;
+export default DeviceSummaryCard
