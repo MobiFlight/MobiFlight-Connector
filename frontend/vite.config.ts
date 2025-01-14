6347f493f39591244ff31a11e1aa9486db06bdf8
Sebastian M <sebastian@mobiflight.com>
2025-01-14 17:11:34 +0000

    Enable correct HMR in devContainer

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // Add this line
+    include: "**/*.tsx",
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    watch: {
+      usePolling: true
+    }
+  }
 })
