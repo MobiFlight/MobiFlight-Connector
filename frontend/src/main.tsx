5551c8e3b046620ed4bfa732acfb30e0c3c5c16b
Sebastian M <sebastian@mobiflight.com>
2025-01-14 20:04:14 +0000

    Add react-router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n.ts'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route index path="/" element={<App />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
