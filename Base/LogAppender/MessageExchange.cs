18eaf21bfe98a20d0d4a7d2493f9eb109691f8c4
Sebastian M <sebastian@mobiflight.com>
2025-01-20 19:52:20 +0100

    MF2025 - Make messaging between Frontend and Backend work (#1925)
    * Delete files that should not be in git
    
    * Init vite / tailwind
    
    * Add Shadcn/ui
    
    * Add playwright
    
    * Added prettier incl tailwind plugin
    
    * Move tailwind layers to index.css
    
    * Added i18next
    
    * Updated readme
    
    * Added zustand and lodash
    
    * Updated readme
    
    * Include i18n correctly
    
    * Enable correct HMR in devContainer
    
    * Add vite options to npm run dev
    
    * Add react-router
    
    * Add WebView2 messaging to frontend
    
    * Add .vite-folder to gitignore file
    
    * Make communication between C# and javascript work
    
    * Fully dynamic frontend messaging implemented
    
    * Create message on Publisher Level
    
    * Add unit tests
    
    * JSON serialization/deserialization happens in MessageExchange
    
    * Unit tests added and working

---
+++ Base/LogAppender/MessageExchange.cs
@@ -0,0 +1,20 @@
+﻿using MobiFlight.BrowserMessages;
+using MobiFlight.BrowserMessages.Outgoing;
+using System;
+
+namespace MobiFlight.Base.LogAppender
+{
+    public class MessageExchange : ILogAppender
+    {   public void log(string message, LogSeverity severity)
+        {
+            var m = new LogEntry
+            {
+                Timestamp = DateTime.Now,
+                Message = message,
+                Severity = severity.ToString()
+            };
+
+            BrowserMessages.MessageExchange.Instance.Publish(m);
+        }
+    }
+}
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,18 @@
+// export type { IConfigItem, IDeviceItem, DeviceType } from "./config"
+// export type { IDeviceElement, DeviceElementType } from "./deviceElements"
+// export type { default as IGlobalSettings } from "./globalsettings"
+export type { LogLevel, ILogMessage } from "./log"
+// export type { Project } from "./project"
+export type {
+  ConfigLoadedEvent,
+  AppMessage,
+  AppMessageKey,
+  AppMessagePayload,
+  StatusBarUpdate,
+  ExecutionUpdate,
+  ConfigValueUpdate,
+  Message,
+  DeviceUpdate,
+  GlobalSettingsUpdateMessage,
+  Notification
+} from "./messages"
