28f574245438b80f44b8adf1d13a0880583decf8
Sebastian M <sebastian@mobiflight.com>
2025-01-23 22:37:24 +0100

    MF2025 - Progress Bar (#1927)
    * Add progress bar
    
    * Reorganize panel in MainForm
    
    * Move the playwright file to .github folder
    
    * Create playwright test
    
    * Update devContainer to support playwright
    
    * Fix branch name

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,53 @@
 import './App.css'
 import { useTranslation } from "react-i18next"
 import { publishOnMessageExchange, useAppMessage } from './lib/hooks/appMessage'
-import { ILogMessage } from './types'
-import { useState } from 'react'
+import { StatusBarUpdate } from './types'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router'
+import StartupProgress from './components/StartupProgress'
 
 function App() {
+  const [queryParameters] = useSearchParams()
   const { t } = useTranslation()
-  const [logEntries, setLogEntries] = useState<ILogMessage[]>([])
 
-  useAppMessage('LogEntry', (log) => {
-    const message = log.payload as ILogMessage
-    setLogEntries((entries) => [...entries, message])
+  const [startupProgress, setStartupProgress] = useState<StatusBarUpdate>(
+    { Value: 0, Text: "Starting..." },
+  )
+
+  useAppMessage("StatusBarUpdate", (message) => {
+    setStartupProgress(message.payload as StatusBarUpdate)
   })
+
+  // this allows to get beyond the startup screen
+  // by setting the progress to 100 via url parameter
+  useEffect(() => {
+    // convert string to number    
+    const value = Number.parseInt(queryParameters.get("progress")?.toString() ?? "0")
+    if (value == 100)
+      setStartupProgress({ Value: 100, Text: "Finished!" })
+    else
+      setStartupProgress({ Value: value , Text: "Loading..." })
+  }, [queryParameters])
 
   const { publish } = publishOnMessageExchange();
   const handleClick = () => {
     publish({ key: 'Test', payload: { 'Message': 'Hello World' } })
   }
+
   return (
     <>
-      <h1>MobiFlight 2025</h1>
-      <h2>{t('app.greeting')}</h2>
-      <p>Test123</p>
-      <button onClick={handleClick}>Test</button>
-      <div>
-        {logEntries.map((entry, index) => (
-          <div key={index}>{entry.Message}</div>
-        ))}
-      </div>
+      {startupProgress.Value < 100 ? (
+        <StartupProgress
+          value={startupProgress.Value}
+          text={startupProgress.Text}
+        />
+      ) : (
+        <><h1>MobiFlight 2025</h1>
+          <h2>{t('app.greeting')}</h2>
+          <p>Test123</p>
+          <button onClick={handleClick}>Test</button>
+        </>
+      )}
     </>
   )
 }
