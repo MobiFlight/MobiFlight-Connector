d42af2fc47e15a265254cec0582734a2cae947f9
Sebastian M <sebastian@mobiflight.com>
2025-04-09 00:07:41 +0200

    Fix column overlap (#2090)
    * Fix long device name overlap
    
    * added background color for separation of information.
    
    * Implementing review feedback
    
    * Fix tailwind color name
    
    * Fix tailwind class typo

--- frontend/src/components/tables/config-item-table/items/ConfigItemTableDeviceCell.tsx
+++ frontend/src/components/tables/config-item-table/items/ConfigItemTableDeviceCell.tsx
@@ -12,49 +12,62 @@ import React from "react"
 interface ConfigItemTableDeviceCellProps {
   row: Row<IConfigItem>
 }
-const ConfigItemTableDeviceCell = React.memo(({ row }: ConfigItemTableDeviceCellProps) => {
-  const { t } = useTranslation()
-  const item = row.original as IConfigItem
-  const Status = item.Status;
-  const Device = Status && !isEmpty(Status["Device"]);
+const ConfigItemTableDeviceCell = React.memo(
+  ({ row }: ConfigItemTableDeviceCellProps) => {
+    const { t } = useTranslation()
+    const item = row.original as IConfigItem
+    const Status = item.Status
+    const Device = Status && !isEmpty(Status["Device"])
 
-  const deviceLabel =
-    (item.Device as IDeviceConfig)?.Name ??
-    (!isEmpty(item.DeviceName) ? item.DeviceName : "-")
-  const type =
-    (item.Device as IDeviceConfig)?.Type ??
-    (!isEmpty(item.DeviceType) ? item.DeviceType : "-")
-  const icon = (
-    <DeviceIcon
-      disabled={!item.Active}
-      variant={(type ?? "default") as DeviceElementType}
-    />
-  )
+    const deviceLabel =
+      (item.Device as IDeviceConfig)?.Name ??
+      (!isEmpty(item.DeviceName) ? item.DeviceName : "-")
+    const type =
+      (item.Device as IDeviceConfig)?.Type ??
+      (!isEmpty(item.DeviceType) ? item.DeviceType : "-")
+    const icon = (
+      <DeviceIcon
+        disabled={!item.Active}
+        variant={(type ?? "default") as DeviceElementType}
+      />
+    )
 
-  const statusIcon = Device ? (<StackedIcons bottomIcon={icon} topIcon={<IconX aria-label="Device" role="status" aria-disabled="false" />} />) : icon
-  const typeLabel = t(
-    `Types.${type?.replace("MobiFlight.OutputConfigItem", "").replace("MobiFlight.InputConfigItem", "")}`,
-  )
+    const statusIcon = Device ? (
+      <StackedIcons
+        bottomIcon={icon}
+        topIcon={
+          <IconX aria-label="Device" role="status" aria-disabled="false" />
+        }
+      />
+    ) : (
+      icon
+    )
+    const typeLabel = t(
+      `Types.${type?.replace("MobiFlight.OutputConfigItem", "").replace("MobiFlight.InputConfigItem", "")}`,
+    )
 
-  const tooltipLabel = Device ? t(`ConfigList.Status.Device.${Status["Device"]}`) : typeLabel
+    const tooltipLabel = Device
+      ? t(`ConfigList.Status.Device.${Status["Device"]}`)
+      : typeLabel
 
-  return type != "-" ? (
-    <ToolTip content={tooltipLabel}>
-      <div className="flex flex-row items-center gap-2">
-        {statusIcon}
-        <div className="hidden flex-col lg:flex">
-          <p className="text-md truncate">{deviceLabel}</p>
+    return type != "-" ? (
+      <ToolTip content={tooltipLabel}>
+        <div className="flex flex-row items-center gap-2">
+          {statusIcon}
+          <span className="text-md hidden truncate lg:inline">
+            {deviceLabel}
+          </span>
         </div>
-      </div>
-    </ToolTip>
-  ) : (
-    <ToolTip content={t("ConfigList.Cell.Device.not set")}>
-    <div className="item-center flex flex-row gap-2 text-slate-400 w-full">
-      <IconBan />
-      <span className="hidden lg:inline">not set</span>
-    </div>
-    </ToolTip>
-  )
-})
+      </ToolTip>
+    ) : (
+      <ToolTip content={t("ConfigList.Cell.Device.not set")}>
+        <div className="flex flex-row items-center gap-2 text-slate-400">
+          <IconBan />
+          <span className="hidden lg:inline">not set</span>
+        </div>
+      </ToolTip>
+    )
+  },
+)
 
 export default ConfigItemTableDeviceCell
