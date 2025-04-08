9a821c6b5ca0417c719b5ee9dd706961ab5c8fd1
Koseng <26673978+Koseng@users.noreply.github.com>
2025-04-08 21:45:16 +0200

    Add WinWing PFP7 support

--- MobiFlight/Joysticks/Winwing/WinwingConstants.cs
+++ MobiFlight/Joysticks/Winwing/WinwingConstants.cs
@@ -15,8 +15,13 @@
         internal const int PRODUCT_ID_PFP3N_OBS = 0xBB39;
         internal const int PRODUCT_ID_PFP3N_FO = 0xBB3D;
 
+        internal const int PRODUCT_ID_PFP7_CPT = 0xBB37;
+        internal const int PRODUCT_ID_PFP7_OBS = 0xBB3B;
+        internal const int PRODUCT_ID_PFP7_FO  = 0xBB3F;
+
         internal static readonly int[] FCU_PRODUCTIDS = { PRODUCT_ID_FCU_ONLY, PRODUCT_ID_FCU_EFISL, PRODUCT_ID_FCU_EFISR, PRODUCT_ID_FCU_EFISL_EFISR };
         internal static readonly int[] CDU_PRODUCTIDS = { PRODUCT_ID_MCDU_CPT, PRODUCT_ID_MCDU_OBS, PRODUCT_ID_MCDU_FO,
-                                                          PRODUCT_ID_PFP3N_CPT, PRODUCT_ID_PFP3N_OBS, PRODUCT_ID_PFP3N_FO };
+                                                          PRODUCT_ID_PFP3N_CPT, PRODUCT_ID_PFP3N_OBS, PRODUCT_ID_PFP3N_FO,
+                                                          PRODUCT_ID_PFP7_CPT, PRODUCT_ID_PFP7_OBS, PRODUCT_ID_PFP7_FO };
     }
 }
