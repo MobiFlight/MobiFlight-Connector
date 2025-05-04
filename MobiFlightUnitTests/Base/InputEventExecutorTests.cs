9da93e009073ae3bcfdba4c2a705120d5df4314c
Sebastian M <sebastian@mobiflight.com>
2025-05-04 11:47:34 +0200

    Add first unit tests

---
+++ MobiFlightUnitTests/Base/InputEventExecutorTests.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobiFlight.Base;
+using MobiFlight.FSUIPC;
+using MobiFlight.InputConfig;
+using MobiFlight.SimConnectMSFS;
+using MobiFlight.xplane;
+using Moq;
+using System.Collections.Generic;
+
+namespace MobiFlight.Execution.Tests
+{
+    [TestClass]
+    public class InputEventExecutorTests
+    {
+        private Mock<InputActionExecutionCache> _mockInputActionExecutionCache;
+        private Mock<Fsuipc2Cache> _mockFsuipcCache;
+        private Mock<SimConnectCache> _mockSimConnectCache;
+        private Mock<XplaneCache> _mockXplaneCache;
+        private Mock<MobiFlightCache> _mockMobiFlightCache;
+        private Mock<JoystickManager> _mockJoystickManager;
+        private Mock<ArcazeCache> _mockArcazeCache;
+        private List<IConfigItem> _configItems;
+        private InputEventExecutor _executor;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _mockInputActionExecutionCache = new Mock<InputActionExecutionCache>();
+            _mockFsuipcCache = new Mock<Fsuipc2Cache>();
+            _mockSimConnectCache = new Mock<SimConnectCache>();
+            _mockXplaneCache = new Mock<XplaneCache>();
+            _mockMobiFlightCache = new Mock<MobiFlightCache>();
+            _mockJoystickManager = new Mock<JoystickManager>();
+            _mockArcazeCache = new Mock<ArcazeCache>();
+
+            _configItems = new List<IConfigItem>();
+            _executor = new InputEventExecutor(
+                _configItems,
+                _mockInputActionExecutionCache.Object,
+                _mockFsuipcCache.Object,
+                _mockSimConnectCache.Object,
+                _mockXplaneCache.Object,
+                _mockMobiFlightCache.Object,
+                _mockJoystickManager.Object,
+                _mockArcazeCache.Object
+            );
+        }
+
+        [TestMethod]
+        public void Execute_NoMatchingConfigItems_ReturnsEmptyDictionary()
+        {
+            // Arrange
+            var inputEventArgs = new InputEventArgs
+            {
+                Serial = "123",
+                Type = DeviceType.Button,
+                DeviceId = "Device1"
+            };
+
+            // Act
+            var result = _executor.Execute(inputEventArgs, isStarted: true);
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Execute_MatchingInactiveConfigItem_SkipsExecution()
+        {
+            // Arrange
+            var inputEventArgs = new InputEventArgs
+            {
+                Serial = "123",
+                Type = DeviceType.Button,
+                DeviceId = "Device1"
+            };
+
+            var inactiveConfigItem = new InputConfigItem
+            {
+                Active = false,
+                ModuleSerial = "/ 123",
+                DeviceName = "Device1"
+            };
+
+            _configItems.Add(inactiveConfigItem);
+
+            // Act
+            var result = _executor.Execute(inputEventArgs, isStarted: true);
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Execute_MatchingActiveConfigItem_ExecutesSuccessfully()
+        {
+            // Arrange
+            var inputEventArgs = new InputEventArgs
+            {
+                Serial = "123",
+                Type = DeviceType.Button,
+                DeviceId = "Device1",
+                Value = 1
+            };
+
+            var activeConfigItem = new InputConfigItem
+            {
+                Active = true,
+                ModuleSerial = "/ 123",
+                DeviceName = "Device1",
+                Name = "TestConfig"
+            };
+
+            _configItems.Add(activeConfigItem);
+
+            // Act
+            var result = _executor.Execute(inputEventArgs, isStarted: true);
+
+            // Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.IsTrue(result.ContainsKey(activeConfigItem.GUID));
+        }
+
+        [TestMethod]
+        public void Execute_PreconditionsNotSatisfied_SkipsExecution()
+        {
+            // Arrange
+            var inputEventArgs = new InputEventArgs
+            {
+                Serial = "123",
+                Type = DeviceType.Button,
+                DeviceId = "Device1",
+                Value = 1
+            };
+
+            var configItem = new InputConfigItem
+            {
+                Active = true,
+                ModuleSerial = "/ 123",
+                DeviceName = "Device1",
+                Name = "TestConfig",
+                Preconditions = new PreconditionList()
+            };
+
+            _configItems.Add(configItem);
+
+            // Mock precondition check to return false
+            _mockArcazeCache.Setup(x => x.clearGetValues());
+
+            // Act
+            var result = _executor.Execute(inputEventArgs, isStarted: true);
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Execute_NotStarted_SkipsExecution()
+        {
+            // Arrange
+            var inputEventArgs = new InputEventArgs
+            {
+                Serial = "123",
+                Type = DeviceType.Button,
+                DeviceId = "Device1",
+                Value = 1
+            };
+
+            var activeConfigItem = new InputConfigItem
+            {
+                Active = true,
+                ModuleSerial = "/ 123",
+                DeviceName = "Device1",
+                Name = "TestConfig"
+            };
+
+            _configItems.Add(activeConfigItem);
+
+            // Act
+            var result = _executor.Execute(inputEventArgs, isStarted: false);
+
+            // Assert
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+}
