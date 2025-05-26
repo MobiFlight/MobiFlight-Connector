c4d6e8aa5d18258c6324de7e5b0d069bd086e431
Sebastian M <sebastian@mobiflight.com>
2025-05-26 22:44:52 +0200

    Improve icon colors, light & dark mode

--- frontend/src/components/tables/config-item-table/items/ConfigItemTableRawValueCell.tsx
+++ frontend/src/components/tables/config-item-table/items/ConfigItemTableRawValueCell.tsx
@@ -1,10 +1,9 @@
 import StatusIcon from "@/components/icons/StatusIcon"
-import ToolTip from "@/components/ToolTip"
+import ValueIcon from "@/components/icons/ValueIcon"
 import { Badge } from "@/components/ui/badge"
 import { IConfigItem } from "@/types"
 import {
   IconBuildingBroadcastTower,
-  IconHourglassEmpty,
 } from "@tabler/icons-react"
 import { Row } from "@tanstack/react-table"
 import { isEmpty } from "lodash-es"
@@ -15,51 +14,49 @@ interface ConfigItemTableRawValueCellProps {
   row: Row<IConfigItem>
 }
 
-const ConfigItemTableRawValueCell = React.memo(({
-  row,
-}: ConfigItemTableRawValueCellProps) => {
-  const item = row.original as IConfigItem
+const ConfigItemTableRawValueCell = React.memo(
+  ({ row }: ConfigItemTableRawValueCellProps) => {
+    const item = row.original as IConfigItem
 
-  const Status = item.Status
-  const Source = Status && !isEmpty(Status["Source"])
+    const Status = item.Status
+    const Source = Status && !isEmpty(Status["Source"])
 
-  const { t } = useTranslation()
-  const label = item.RawValue
+    const { t } = useTranslation()
+    const label = item.RawValue
 
-  return (
-    <div className="text-md truncate" title="RawValue">
-      {!isEmpty(label) && !Source ? (
-        item.Type == "InputConfigItem" ? (
+    return (
+      <div className="text-md truncate" title="RawValue">
+        {!isEmpty(label) && !Source ? (
+          item.Type == "InputConfigItem" ? (
+            <div className="flex flex-row justify-center">
+              <Badge variant="secondary">
+                {label?.replace("CHANGE =>", "")}
+              </Badge>
+            </div>
+          ) : (
+            <div className="text-sm">
+              <>{label}</>
+            </div>
+          )
+        ) : Source ? (
           <div className="flex flex-row justify-center">
-            <Badge variant="secondary">{label?.replace("CHANGE =>", "")}</Badge>
+            <StatusIcon
+              status="Source"
+              condition={Source}
+              title={
+                Source
+                  ? t(`ConfigList.Status.Source.${Status["Source"]}`)
+                  : "available"
+              }
+              IconComponent={IconBuildingBroadcastTower}
+            />
           </div>
         ) : (
-          <div className="text-sm">
-            <>{label}</>
-          </div>
-        )
-      ) : Source ? (
-        <div className="flex flex-row justify-center">
-          <StatusIcon
-            status="Source"
-            condition={Source}
-            title={
-              Source
-                ? t(`ConfigList.Status.Source.${Status["Source"]}`)
-                : "available"
-            }
-            IconComponent={IconBuildingBroadcastTower}
-          />
-        </div>
-      ) : (
-        <div className="flex flex-row justify-center text-slate-200">
-          <ToolTip content={t("ConfigList.Cell.Waiting")}>
-            <IconHourglassEmpty className="h-7 w-7"/>
-          </ToolTip>
-        </div>
-      )}
-    </div>
-  )
-})
+          <ValueIcon />
+        )}
+      </div>
+    )
+  },
+)
 
 export default ConfigItemTableRawValueCell
