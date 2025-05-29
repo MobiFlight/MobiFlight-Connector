dfa6d5c767143178c66f45bb10d949f5b6c539a9
Sebastian M <sebastian@mobiflight.com>
2025-05-29 09:10:23 +0000

    Make icon smaller for light/dark mode switch

--- frontend/src/components/DarkModeToggle.tsx
+++ frontend/src/components/DarkModeToggle.tsx
@@ -7,12 +7,14 @@ const DarkModeToggle = () => {
   return (
     <div className="hidden md:flex items-center relative">
       <IconMoon
+        size={18}
         role="button"
         aria-label="Toggle light mode"
         className="cursor-pointer rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
         onClick={() => setTheme("light")}
         />
       <IconSun
+        size={18}
         role="button"
         aria-label="Toggle dark mode"
         className="cursor-pointer absolute rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
