5a6e9e8fa4645a8f0e510d84416616a175d066dd
Sebastian M <sebastian@mobiflight.com>
2025-06-12 15:06:23 +0200

    Remove unused `using`

--- MobiFlight/InputConfig/InputActionFactory.cs
+++ MobiFlight/InputConfig/InputActionFactory.cs
@@ -1,9 +1,5 @@
-﻿using MobiFlight.InputConfig;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
 namespace MobiFlight.InputConfig
