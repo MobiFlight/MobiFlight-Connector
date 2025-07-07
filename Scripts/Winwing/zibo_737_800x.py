18965ab57ccb6bdd94b2ba257188c2f53a5f806e
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2025-07-07 21:49:59 +0100

    Logging, header description and unicode characters

--- Scripts/Winwing/zibo_737_800x.py
+++ Scripts/Winwing/zibo_737_800x.py
@@ -1,6 +1,28 @@
+"""
+Adds support for the Zibo 737 in X-Plane
+
+Many X-Plane aircraft have similar formats for datarefs and the means of retrieving, translating and sending updates is mostly the same.
+
+In order to support multiple CDU devices seamlessly, a dynamic approach is taken whereby an enum class is defined that contains the supported devices.
+A device is considered "supported" if it exists in the aircraft. Some aircraft have 3 CDUs while others have 2.
+Each enum member is assigned a value that represents the X-Plane dataref identifier. Example: fmc1 of laminar/B738/fmc1/Line04_I.
+
+Upon script start, Mobiflight is probed (get_available_devices()) to detect the devices connected to the PC. Any device that returns a successful response is then tracked.
+
+Two tasks are started independently for each avialable CDU device.
+1. handle_dataref_updates -> Listens to X-Plane's WebSocket server for dataref updates for that specific CDU and pushes an event to a queue
+2. handle_device_update   -> Listens to the queue and dispatches updates to Mobiflight to update that CDU
+
+Tasks are started independently for each CDU device to ensure each device can update quickly, particularly when players might be performing shared cockpit flights.
+
+Upon a failed connection while dispatching updates to Mobiflight, the handle_device_update function use `async for` with the websockets client. The failed message is put back in the queue, the loop continues to the next iteration which then reconnects again.
+The failed message is picked back up and dispatched to Mobiflight. This ensures a user's device eventually receives the updated display contents and doesn't hang which would require the user to cycle the page again.
+"""
+
 import asyncio
 import base64
 import json
+import logging
 import urllib.request
 import websockets
 from enum import StrEnum
@@ -18,7 +40,7 @@ BASE_WEBSOCKET_URI = f"ws://{WEBSOCKET_HOST}:8086/api/v2"
 WS_CAPTAIN = f"ws://{WEBSOCKET_HOST}:{WEBSOCKET_PORT}/winwing/cdu-captain"
 WS_CO_PILOT = f"ws://{WEBSOCKET_HOST}:{WEBSOCKET_PORT}/winwing/cdu-co-pilot"
 
-CHARACTER_MAPPING = {"`": "\u00b0", "*": "\u2610", "=": "\u002a"}
+CHARACTER_MAPPING = {"`": "°", "*": "☐", "=": "*"}
 COLOR_MAPPING = {"G": "g", "C": "c", "I": "e", "M": "m"}
 
 
@@ -137,17 +159,25 @@ def generate_display_json(values: dict[str, str]) -> str:
 
 
 async def handle_device_update(queue: asyncio.Queue, device: CduDevice):
+    """
+    Translates and sends dataref updates to Mobiflight.
+    """
     last_run_time = 0
     rate_limit_time = 0.1
 
     endpoint = device.get_endpoint()
+    logging.info("Connecting to CDU device %s", device)
     async for websocket in websockets.connect(endpoint):
+        logging.info("Connected successfully to CDU device %s", device)
         while True:
             values = await queue.get()
 
             try:
                 elapsed = asyncio.get_event_loop().time() - last_run_time
 
+                # Weaker CPUs may experience performance issues when a websocket connection is saturated with requests, such as when pages are frequently changed.
+                # This rate limits the number of active websocket requests to Mobiflight.
+                # The delay should not be noticeable unless a user heavily spams page changes, but it should be enough that too many messages won't be pushed at once.
                 if elapsed < rate_limit_time:
                     await asyncio.sleep(rate_limit_time - elapsed)
 
@@ -156,6 +186,9 @@ async def handle_device_update(queue: asyncio.Queue, device: CduDevice):
                 last_run_time = asyncio.get_event_loop().time()
 
             except websockets.exceptions.ConnectionClosed:
+                logging.error(
+                    "Mobiflight websocket connection was closed... Attempting to reconnect"
+                )
                 await queue.put(values)
                 break
 
@@ -164,9 +197,11 @@ async def handle_dataref_updates(queue: asyncio.Queue, device: CduDevice):
     last_known_values = {}
 
     dataref_map = fetch_dataref_mapping(device)
+    logging.info("Connecting to X-Plane websocket server")
     async for websocket in websockets.connect(
         BASE_WEBSOCKET_URI,
     ):
+        logging.info("Connected successfully to X-Plane websocket server")
         try:
             await websocket.send(
                 json.dumps(
@@ -207,6 +242,9 @@ async def handle_dataref_updates(queue: asyncio.Queue, device: CduDevice):
                 last_known_values = new_values
                 await queue.put(new_values)
         except websockets.exceptions.ConnectionClosed:
+            logging.error(
+                "X-Plane websocket connection was closed... Attempting to reconnect"
+            )
             continue
 
 
@@ -215,12 +253,22 @@ async def get_available_devices() -> list[CduDevice]:
 
     available_devices = []
 
+    logging.info("Checking Mobiflight for available CDU devices")
     for device in device_candidates:
+        device_endpoint = device.get_endpoint()
         try:
-            async with websockets.connect(device.get_endpoint()) as socket:
+            async with websockets.connect(device_endpoint) as socket:
+                logging.info(
+                    "Discovered CDU device %s at endpoint %s", device, device_endpoint
+                )
                 available_devices.append(device)
                 await socket.send(FONT_REQUEST)
         except websockets.WebSocketException:
+            logging.warning(
+                "Attempted to probe CDU device %s at endpoint %s but device wasn't available",
+                device,
+                device_endpoint,
+            )
             continue
 
     return available_devices
@@ -235,6 +283,8 @@ async def main():
 
         tasks.append(asyncio.create_task(handle_dataref_updates(queue, device)))
         tasks.append(asyncio.create_task(handle_device_update(queue, device)))
+
+    logging.info("Started background tasks for %s", available_devices)
 
     await asyncio.gather(*tasks)
 
