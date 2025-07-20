3b6886de33bc14f8a0278c645569b7f62a7766f5
Sebastian M <sebastian@mobiflight.com>
2025-07-20 22:53:37 +0000

    Use ToolTip instead of Tooltip

--- frontend/src/components/ExecutionToolbar.tsx
+++ frontend/src/components/ExecutionToolbar.tsx
@@ -11,11 +11,7 @@ import { CommandProjectToolbarPayload } from "@/types/commands"
 import { ExecutionState } from "@/types/messages"
 import IconAutoRun from "./icons/IconAutoRun"
 import TwoStateIcon from "./icons/TwoStateIcon"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "./ui/tooltip"
+import ToolTip from "./ToolTip"
 
 export const ExecutionToolbar = () => {
   const { settings } = useSettingsStore()
@@ -39,69 +35,67 @@ export const ExecutionToolbar = () => {
 
   return (
     <div className="flex items-center gap-2" role="toolbar">
-      <Tooltip>
-        <TooltipTrigger asChild>
+      <ToolTip
+        content={settings?.AutoRun ? "Disable Auto Run" : "Enable Auto Run"}
+      >
+        <Button
+          variant="ghost"
+          className="text-md h-8 gap-1 p-1 [&_svg]:size-6"
+          onClick={() => handleMenuItemClick({ action: "toggleAutoRun" })}
+        >
+          <IconAutoRun
+            className={
+              settings?.AutoRun
+                ? "stroke-yellow-500 transition-colors"
+                : "stroke-muted-foreground transition-colors"
+            }
+          />
+          <div className="hidden lg:inline-flex pr-1">Auto</div>
+        </Button>
+      </ToolTip>
+
+      <ToolTip
+        content={
+          isRunning
+            ? "Stop run mode"
+            : isTesting
+              ? "Test mode active. Stop it first."
+              : "Start run mode and process all configs"
+        }
+      >
+        <div className="inline-flex">
           <Button
+            disabled={isTesting}
             variant="ghost"
             className="text-md h-8 gap-1 p-1 [&_svg]:size-6"
-            onClick={() => handleMenuItemClick({ action: "toggleAutoRun" })}
+            onClick={() =>
+              handleMenuItemClick({ action: !isRunning ? "run" : "stop" })
+            }
           >
-            <IconAutoRun
-              className={
-                settings?.AutoRun
-                  ? "stroke-yellow-500 transition-colors"
-                  : "stroke-muted-foreground transition-colors"
+            <TwoStateIcon
+              state={isRunning}
+              primaryIcon={IconPlayerPlayFilled}
+              secondaryIcon={IconPlayerStopFilled}
+              primaryClassName={
+                !isTesting
+                  ? "fill-green-600 stroke-green-600"
+                  : "fill-none stroke-2 stroke-muted-foreground"
               }
+              secondaryClassName="fill-red-700 stroke-red-700"
             />
-            <div className="hidden lg:inline-flex">Auto</div>
+            <div className="hidden lg:inline-flex pr-1">
+              {!isRunning ? "Run" : "Stop"}
+            </div>
           </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>{settings?.AutoRun ? "Disable Auto Run" : "Enable Auto Run"}</p>
-        </TooltipContent>
-      </Tooltip>
+        </div>
+      </ToolTip>
 
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <div className="inline-flex">
-            <Button
-              disabled={isTesting}
-              variant="ghost"
-              className="text-md h-8 gap-1 p-1 [&_svg]:size-6"
-              onClick={() =>
-                handleMenuItemClick({ action: !isRunning ? "run" : "stop" })
-              }
-            >
-              <TwoStateIcon
-                state={isRunning}
-                primaryIcon={IconPlayerPlayFilled}
-                secondaryIcon={IconPlayerStopFilled}
-                primaryClassName={
-                  !isTesting
-                    ? "fill-green-600 stroke-green-600"
-                    : "fill-none stroke-2 stroke-muted-foreground"
-                }
-                secondaryClassName="fill-red-700 stroke-red-700"
-              />
-              <div className="hidden lg:inline-flex">
-                {!isRunning ? "Run" : "Stop"}
-              </div>
-            </Button>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>
-            {isRunning
-              ? "Stop run mode"
-              : isTesting
-                ? "Test mode active. Stop it first."
-                : "Start run mode and process all configs"}
-          </p>
-        </TooltipContent>
-      </Tooltip>
-
-      <Tooltip>
-        <TooltipTrigger asChild>
+      <ToolTip
+        content={isTesting
+              ? "Stop test mode execution"
+              : isRunning
+                ? "Run mode active. Stop it first."
+                : "Start test mode and test all configs"}>
           <div className="inline-flex">
             <Button
               disabled={isRunning}
@@ -122,22 +116,12 @@ export const ExecutionToolbar = () => {
                 }
                 secondaryClassName="fill-red-700 stroke-red-700"
               />
-              <div className="hidden lg:inline-flex">
+              <div className="hidden lg:inline-flex pr-1">
                 {!isTesting ? "Test" : "Stop"}
               </div>
             </Button>
           </div>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>
-            {isTesting
-              ? "Stop test mode execution"
-              : isRunning
-                ? "Run mode active. Stop it first."
-                : "Start test mode and test all configs"}
-          </p>
-        </TooltipContent>
-      </Tooltip>
+      </ToolTip>
     </div>
   )
 }
