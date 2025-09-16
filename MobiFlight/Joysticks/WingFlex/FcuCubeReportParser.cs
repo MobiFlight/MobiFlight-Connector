1d9c7817051cf36e4594470a3361acd3207609b6
Sebastian M <sebastian@mobiflight.com>
2025-09-16 16:38:58 +0200

    Set Buffer Header correctly

--- MobiFlight/Joysticks/WingFlex/FcuCubeReportParser.cs
+++ MobiFlight/Joysticks/WingFlex/FcuCubeReportParser.cs
@@ -1,12 +1,62 @@
 ﻿using SharpDX.DirectInput;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace MobiFlight.Joysticks.WingFlex
 {
     internal class FcuCubeReport
     {
+        // -                Head                         Constant: 0xF2                          -       0       -       0xF2
+        // -                Head                         Constant: 0xE1                          -       1       -       0xE1
+        // -                Head                         Constant: 0x03                          -       2       -       0x03
+        private static byte[] InputHeader = new byte[] { 0xF2, 0xE1, 0x03 };
+        // -                Data Type Total              Has 2 Data Type                         -       3       -       0x02
+        // -                Data Type                    Bit Type                                -       4       -       0x01
+        // -                Data Length                  Following data occupies 2 Bytes         -       5       -       0x02
+        private static byte[] InputBitSection = new byte[] { 0x02, 0x01, 0x02 };
+        // -                Data Type                    Single Byte Type                        -       9       -       0x02
+        // -                Data Length                  Following data occupies 6 Bytes         -       10      -       0x06
+        private static byte[] InputByteSection = new byte[] { 0x02, 0x06 };
+        
+        // -                Head                         Constant: 0xF2                          -       0       -       0xF2
+        // -                Head                         Constant: 0xE1                          -       1       -       0xE1
+        // -                Head                         Constant: 0x03                          -       2       -       0x03
+        private static byte[] OutputHeader = new byte[] { 0xF2, 0xE1, 0x03 };
+        // -                Data Type Total              Has 3 Data Type                         -       3       -       0x02
+        // -                Data Type                    Bit Type                                -       4       -       0x01
+        // -                Data Length                  Following data occupies 3 Bytes         -       5       -       0x02
+        private static byte[] OutputBitSection = new byte[] { 0x02, 0x01, 0x02 };
+        // -                Data Type                    Single Byte Type                        -       9       -       0x02
+        // -                Data Length                  Following data occupies 2 Bytes         -       10      -       0x02
+        // Output           Background Light Brightness  0x00(Minimum)~0xFF(Maximum)             -       11      -       0
+        // Output           LCD Brightness               0x00(Minimum)~0xFF(Maximum)             -       12      -       0
+        //                  Data Type                    Double Byte Type                        -       13      -       0
+        //                  Data Length                  Following data occupies 8 Bytes         -       14      -       0x08
+        private static byte[] OutputByteSection = new byte[] { 0x02, 0x02, 0, 0, 0, 0x08 };
+
+
         private byte[] LastInputBufferState = new byte[16];
         private byte[] LastOutputBufferState = new byte[22];
+
+        public FcuCubeReport() {
+            InitLastInputBufferState();
+            InitLastOutputBufferState();
+        }
+        private void InitLastInputBufferState()
+        {
+            Buffer.BlockCopy(InputHeader, 0, LastOutputBufferState, 0, InputHeader.Length);
+            Buffer.BlockCopy(OutputBitSection, 0, LastOutputBufferState, 3, OutputBitSection.Length);
+            Buffer.BlockCopy(OutputByteSection, 0, LastOutputBufferState, 9, OutputByteSection.Length);
+        }
+
+        private void InitLastOutputBufferState()
+        {
+            // Copy using Buffer.BlockCopy - fastest method
+            Buffer.BlockCopy(OutputHeader, 0, LastOutputBufferState, 0, OutputHeader.Length);
+            Buffer.BlockCopy(OutputBitSection, 0, LastOutputBufferState, 3, OutputBitSection.Length);
+            Buffer.BlockCopy(OutputByteSection, 0, LastOutputBufferState, 9, OutputByteSection.Length);
+        }
 
         public void CopyFromInputBuffer(byte[] inputBuffer)
         {
