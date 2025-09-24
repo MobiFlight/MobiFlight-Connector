53e4efc434c527f275d890ba778928bb2f9424d7
Sebastian M <sebastian@mobiflight.com>
2025-09-24 10:06:29 +0200

    Add comments to help with refactoring later.

--- MobiFlight/Joysticks/WingFlex/FcuCube.cs
+++ MobiFlight/Joysticks/WingFlex/FcuCube.cs
@@ -1,7 +1,6 @@
 ﻿using Device.Net;
 using Hid.Net;
 using Hid.Net.Windows;
-using MobiFlight.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,27 +11,64 @@ namespace MobiFlight.Joysticks.WingFlex
 {
     internal class FcuCube : Joystick
     {
+        /// <summary>
+        /// Used for reading HID reports in a background thread.
+        /// </summary>
         bool DoReadHidReports = false;
+
+        /// <summary>
+        /// The thread that reads HID reports.
+        /// </summary>
         private Thread readThread;
+
+        /// <summary>
+        /// The specific HID device instance.
+        /// This is using the Device.Net library for HID communication.
+        /// It provides improved performance compared to HidSharp
+        /// </summary>
         IHidDevice Device { get; set; }
 
+        /// <summary>
+        /// The FCU Cube needs to store the output state of those devices
+        /// that are explicityly set.
+        /// </summary>
         private List<JoystickOutputDevice> OutputState = new List<JoystickOutputDevice>();
 
+        /// <summary>
+        /// The report implementation for the FCU Cube.
+        /// </summary>
         private readonly FcuCubeReport FcuCubeReport = new FcuCubeReport();
+
+        /// <summary>
+        /// Provide same instance name as defined in the definition file.
+        /// Also works if Defintion file is not set yet.
+        /// </summary>
         public override string Name
         {
             get { return Definition?.InstanceName ?? "FcuCube"; }
         }
 
+        /// <summary>
+        /// Provides Serial including prefix.
+        /// Serial information is provided through Device.Net
+        /// </summary>
         public override string Serial
         {
             get { return $"{Joystick.SerialPrefix}{Device?.ConnectedDeviceDefinition?.SerialNumber}" ?? "FCU-CUBE-1234-ABCD-12345678"; }
         }
 
+        /// <summary>
+        /// The constructor.
+        /// </summary>
+        /// <param name="definition">joystick definition file.</param>
         public FcuCube(JoystickDefinition definition) : base(null, definition)
         {
         }
 
+        /// <summary>
+        /// This creates a connection to the HID device using the Device.Net library.
+        /// </summary>
+        /// <returns></returns>
         protected async Task<bool> Connect()
         {
             var VendorId = Definition.VendorId;
@@ -62,6 +98,9 @@ namespace MobiFlight.Joysticks.WingFlex
             return true;
         }
 
+        /// <summary>
+        /// Method called by read thread
+        /// </summary>
         private void ReadHidReportsLoop()
         {
             while (DoReadHidReports)
@@ -81,6 +120,10 @@ namespace MobiFlight.Joysticks.WingFlex
             }
         }
 
+        /// <summary>
+        /// Update is called by the base class
+        /// It is currently needed to ensure that the hid device is correctly initialized.
+        /// </summary>
         public override void Update()
         {
             if (Device == null || !Device.IsInitialized)
@@ -90,6 +133,15 @@ namespace MobiFlight.Joysticks.WingFlex
             }
         }
 
+        /// <summary>
+        /// This processes the input report buffer, triggers button events and stores the state
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// This could be done in the base class.
+        /// </remarks>
+        /// <param name="reportId"></param>
+        /// <param name="inputReportBuffer"></param>
         protected void ProcessInputReportBuffer(byte reportId, byte[] inputReportBuffer) {
             var newState = FcuCubeReport.Parse(inputReportBuffer).ToJoystickState();
             UpdateButtons(newState);
@@ -98,12 +150,21 @@ namespace MobiFlight.Joysticks.WingFlex
             State = newState;
         }
 
+        /// <summary>
+        /// Sends out the data to the device as correct output report.
+        /// </summary>
+        /// <param name="data"></param>
         protected async override void SendData(byte[] data)
         {
             await Device.WriteReportAsync(data, 0).ConfigureAwait(false);
             RequiresOutputUpdate = false;
         }
 
+        /// <summary>
+        /// Sets the actual output device state for simple outputs
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="state"></param>
         public override void SetOutputDeviceState(string name, byte state)
         {
             var light = Lights.FirstOrDefault(l => l.Label == name);
@@ -125,6 +186,11 @@ namespace MobiFlight.Joysticks.WingFlex
             return;
         }
 
+        /// <summary>
+        /// Sets the actual output device state for lcd outputs
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="value"></param>
         public override void SetLcdDisplay(string address, string value)
         {
             var display = Lights.Find(l => l.Name == address) as JoystickOutputDisplay;
@@ -145,6 +211,16 @@ namespace MobiFlight.Joysticks.WingFlex
             RequiresOutputUpdate = true;
         }
 
+        /// <summary>
+        /// Updates the state of the output device by sending the current output data.
+        /// </summary>
+        /// <remarks>This method retrieves the output device state and sends it to the device.  
+        /// 
+        /// It has to be called regularly from an external caller.
+        /// 
+        /// If the output data is unavailable, the method exits without performing any action.  
+        /// If the device is removed during the operation, an <see cref="System.IO.IOException"/> is caught, 
+        /// and the `OnDeviceRemoved` method is invoked.</remarks>
         public override void UpdateOutputDeviceStates()
         {
             // if (!RequiresOutputUpdate) return;
@@ -163,6 +239,12 @@ namespace MobiFlight.Joysticks.WingFlex
             }
         }
 
+        /// <summary>
+        /// Enumerates and categorizes joystick devices based on their type.
+        /// </summary>
+        /// <remarks>This method processes the joystick device definitions and categorizes them into 
+        /// analog inputs, buttons, or POV controls. Devices are added to their respective  collections based on their
+        /// type.</remarks>
         protected override void EnumerateDevices()
         {
             Definition.Inputs.ForEach(d => { 
@@ -185,6 +267,12 @@ namespace MobiFlight.Joysticks.WingFlex
             });
         }
 
+        /// <summary>
+        /// Enumerates and initializes the output devices associated with the current instance.
+        /// </summary>
+        /// <remarks>This method identifies output devices of type <see cref="DeviceType.LcdDisplay"/> and
+        /// adds them to the collection of lights as <see cref="JoystickOutputDisplay"/> instances. The method relies on
+        /// the `this.Definition` <see cref="JoystickDefinition"/> property to retrieve device information.</remarks>
         protected override void EnumerateOutputDevices()
         {
             base.EnumerateOutputDevices();
@@ -196,6 +284,16 @@ namespace MobiFlight.Joysticks.WingFlex
             });
         }
 
+        /// <summary>
+        /// Retrieves a collection of distinct output device types that are currently connected.
+        /// </summary>
+        /// <remarks>
+        /// The method examines the outputs defined in the Defintiion property (<see cref="JoystickDefinition"/>) and
+        /// determines the corresponding device types. If an output does not specify a type, it is categorized as <see
+        /// cref="DeviceType.Output"/>. Duplicate device types are excluded from the result.
+        /// </remarks>
+        /// <returns>An <see cref="IEnumerable{T}"/> of <see cref="DeviceType"/> representing the distinct types of connected
+        /// output devices. The collection will be empty if no outputs are defined or connected.</returns>
         public override IEnumerable<DeviceType> GetConnectedOutputDeviceTypes()
         {
             List<DeviceType> result = new List<DeviceType>();
@@ -216,13 +314,21 @@ namespace MobiFlight.Joysticks.WingFlex
             return result;
         }
 
+        /// <summary>
+        /// Cleans up any specific resources, e.g. thread and device connection.
+        /// </summary>
         public override void Shutdown()
         {
             DoReadHidReports = false;
             readThread?.Join(1000);
             Device?.Dispose();
+
+            base.Shutdown();
         }
 
+        /// <summary>
+        /// Resets all outputs to a "stop" state
+        /// </summary>
         public override void Stop()
         {
             // Reset all outputs to initial state
