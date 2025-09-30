5f96984e1800a781c3c63c3b52f6060d76b3e627
Sebastian M <sebastian@mobiflight.com>
2025-10-01 00:18:50 +0200

    Added i18n for overlay

--- frontend/src/components/tables/config-item-table/LoaderOverlay.tsx
+++ frontend/src/components/tables/config-item-table/LoaderOverlay.tsx
@@ -6,6 +6,7 @@ import {
 
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { LoaderIcon } from "lucide-react"
+import { useTranslation } from "react-i18next"
 
 type LoaderOverlayProps = {
   open: boolean
@@ -14,12 +15,12 @@ type LoaderOverlayProps = {
 
 const LoaderOverlay = (props: LoaderOverlayProps) => {
   const onOpenChange = props.onOpenChange ? props.onOpenChange : () => {}
-
+  const { t } = useTranslation()
   return (
     <>
       <Dialog open={props.open} onOpenChange={onOpenChange}>
         <DialogPortal>
-          <DialogOverlay
+          <DialogOverlay data-testid="loader-overlay"
             onClick={() => {
               onOpenChange(false)
             }}
@@ -27,7 +28,7 @@ const LoaderOverlay = (props: LoaderOverlayProps) => {
           <DialogPrimitive.Content className="fixed left-[50%] top-[50%] z-51 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 focus:outline-none">
             <div className="h-full w-full flex items-center justify-center flex-col gap-2">
               <LoaderIcon className="animate-spin text-background dark:text-foreground" />
-              <p className="text-background dark:text-foreground">Showing wizard...</p>
+              <p className="text-background dark:text-foreground">{t("General.Overlay.OpeningWizard")}</p>
             </div>
           </DialogPrimitive.Content>
         </DialogPortal>
