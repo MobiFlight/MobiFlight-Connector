a0d904302553561ea2d218f34cbcff86194ad714
Sebastian M <sebastian@mobiflight.com>
2025-10-13 12:51:33 +0000

    Changed snap drag style and visual representation

--- frontend/src/components/dnd/ConfigItemDragOverlay.tsx
+++ frontend/src/components/dnd/ConfigItemDragOverlay.tsx
@@ -2,8 +2,7 @@ import { DragOverlay } from "@dnd-kit/core"
 import { useConfigItemDragContext } from "@/lib/hooks/useConfigItemDragContext"
 import ConfigItemTabDragOverlay from "./ConfigItemTabDragOverlay"
 import { cn } from "@/lib/utils"
-import { Badge } from "../ui/badge"
-import { IconExclamationCircle } from "@tabler/icons-react"
+import ConfigItemTableDragOverlay from "./ConfigItemTableDragOverlay"
 
 export function ConfigItemDragOverlay() {
   const { dragState } = useConfigItemDragContext()
@@ -17,37 +16,15 @@ export function ConfigItemDragOverlay() {
     return null
   }
 
-  const itemCount = dragState?.items.draggedItems.length ?? 0
-
   return (
     isDraggingWithItems && (
       <DragOverlay className="cursor-grabbing">
         <div>
-          {isInsideTable && itemCount > 1 ? (
-            <div
-              data-testid="config-item-drag-overlay-inside-table"
-              className={cn(
-                "border-border bg-background relative flex items-center justify-between rounded-lg border px-4 py-3",
-                // Add stacked effect for multiple items using pseudo-elements
-                itemCount > 1 && [
-                  "before:bg-background before:border-border before:absolute before:inset-0 before:-z-10 before:translate-y-1 before:rounded-md before:border",
-                  itemCount > 2 &&
-                    "after:bg-background after:border-border after:absolute after:inset-0 after:-z-20 after:translate-y-2 after:rounded-md after:border",
-                ],
-                "after:shadow-lg",
-              )}
-            >
-              <div>
-                <Badge className="ml-6 px-4 text-xs">{itemCount}</Badge>
-                <span className="px-7 font-medium">
-                  You are moving {itemCount} item(s)
-                </span>
-              </div>
-              <div className="text-muted-foreground ml-4 flex items-center gap-2 text-sm">
-                <IconExclamationCircle className="stroke-muted-foreground/80 h-6 w-6" />
-                You can drag & drop items here in the list or move them to other tabs
-              </div>
-            </div>
+          {isInsideTable ? (
+            <ConfigItemTableDragOverlay 
+              data-testid="config-item-drag-overlay-inside-table" 
+              items={dragState?.items.draggedItems ?? []} 
+            />
           ) : (
             <ConfigItemTabDragOverlay
               data-testid="config-item-drag-overlay-outside-table"
--- frontend/src/components/tables/config-item-table/DndTableRow.tsx
+++ frontend/src/components/tables/config-item-table/DndTableRow.tsx
@@ -25,6 +25,7 @@ export const DndTableRow: React.FC<DndTableRowProps> = ({
     zIndex: 1000,
   }
 
+  const multiDrag = (dragState?.items?.draggedItems?.length ?? 0) > 1
   const isDragging =
     dragState?.items?.draggedItems
       ?.map((item) => item.GUID)
@@ -33,18 +34,20 @@ export const DndTableRow: React.FC<DndTableRowProps> = ({
   const isInTable = dragState?.ui.isInsideTable ?? true
   const dragStyle = isDragging
     ? isActive
-      ? "opacity-100"
+      ? !multiDrag 
+        ? "opacity-0" 
+        : "opacity-0"
       : "opacity-0 collapse"
     : ""
   const outsideTableStyle = !isInTable && isDragging ? "opacity-0 collapse" : ""
-
+  
   return (
     <tr
       style={dndStyle}
       ref={setNodeRef}
       {...listeners}
       className={cn(
-        "group/row hover:bg-selected data-[state=selected]:bg-selected data-[state=selected]:hover:bg-selected dark:data-[state=selected]:bg-selected dark:data-[state=selected]:hover:bg-selected border-b transition-colors",
+        "group/row bg-white hover:bg-selected data-[state=selected]:bg-selected data-[state=selected]:hover:bg-selected dark:data-[state=selected]:bg-selected dark:data-[state=selected]:hover:bg-selected border-b transition-colors",
         dragStyle,
         outsideTableStyle,
         className,
