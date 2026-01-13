2d80170774c9a46ff3b7907528e49f29654501eb
Sebastian M <sebastian@mobiflight.com>
2026-01-13 19:53:32 +0100

    Show actual controller binding data

--- frontend/src/components/modals/ControllerBindingsModal.tsx
+++ frontend/src/components/modals/ControllerBindingsModal.tsx
@@ -1,46 +1,10 @@
 import ControllerBindings from "@/components/controllers/ControllerBindings"
-// import { useProjectStore } from "@/stores/projectStore"
-import { Controller, ControllerBinding } from "@/types/controller"
+import { useProjectStore } from "@/stores/projectStore"
+import { Controller } from "@/types/controller"
 
 const ControllerBindingsModal = () => {
-  // const { project } = useProjectStore()
-  const bindings = [
-    {
-      "BoundController": "miniCOCKPIT miniFCU/ SN-E98-277",
-      "Status": "Match",
-      "OriginalController": "miniCOCKPIT miniFCU/ SN-E98-277"
-    },
-    {
-      "BoundController": "ProtoBoard-v2/ SN-5FC-1CF",
-      "Status": "AutoBind",
-      "OriginalController": "ProtoBoard-v2/ SN-3F1-FDD"
-    },
-    {
-      "BoundController": "Alpha Flight Controls / JS-b0875190-3b89-11ed-8007-444553540000",
-      "Status": "RequiresManualBind",
-      "OriginalController": "Alpha Flight Controls / JS-b0875190-3b89-11ed-8007-444553540000"
-    },
-    {
-      "BoundController": null,
-      "Status": "Missing",
-      "OriginalController": "Bravo Throttle Quadrant / JS-b0875190-3b89-11ed-8007-444553540000"
-    },
-    {
-      "BoundController": null,
-      "Status": "Missing",
-      "OriginalController": "Behringer X-Touch Mini / MI-b0875190-3b89-11ed-8007-444553540001"
-    },
-    {
-      "BoundController": "Generic Gamepad / JS-b0875190-3b89-11ed-8007-444553540002",
-      "Status": "Match",
-      "OriginalController": "Generic Gamepad / JS-b0875190-3b89-11ed-8007-444553540002"
-    },
-    {
-      "BoundController": "Octavi / JS-b0875190-3b89-11ed-8007-44455354003",
-      "Status": "Match",
-      "OriginalController": "Octavi / JS-b0875190-3b89-11ed-8007-44455354003"
-    }  
-  ] as ControllerBinding[]
+  const { project } = useProjectStore()
+  const bindings = project?.ControllerBindings || []
   
   const connectedControllers : Controller[] = [
     { Name: "Prototyping Board", Serial: "SN-5FC-1CF", Vendor: "MobiFlight", ProductId: "", VendorId: "", Type: "MobiFlight", Connected: true, ImageUrl: null, certified: false },
