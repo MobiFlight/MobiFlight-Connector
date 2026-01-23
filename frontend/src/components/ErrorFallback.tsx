955e362540a257126a4fb6803f6eb24d303be0cb
Sebastian M <sebastian@mobiflight.com>
2026-01-23 11:12:26 +0100

    Using actual version info from installation folder

---
+++ frontend/src/components/ErrorFallback.tsx
@@ -0,0 +1,22 @@
+import { IconMoodConfuzed } from "@tabler/icons-react"
+import { FallbackProps } from "react-error-boundary"
+import { useTranslation } from "react-i18next"
+
+const ErrorFallback = ({ error }: FallbackProps) => {
+  const { t } = useTranslation()
+  const errorMessage =
+    (error as Error).message ||
+    error?.toString() ||
+    t("General.Error.Unknown")
+
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center p-4 text-red-500">
+      <div className="flex flex-row items-center gap-4">
+        <IconMoodConfuzed />
+        <p className="text-xl font-bold">{t("General.Error.FallbackTitle")}</p>
+      </div>
+      <pre className="wrap-break-word whitespace-pre-wrap">{errorMessage}</pre>
+    </div>
+  )
+}
+export default ErrorFallback
