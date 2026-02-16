da56b3505f27838bdaaaaca2078e16b1dddc37f1
Sebastian M <sebastian@mobiflight.com>
2026-02-16 00:06:50 +0000

    Add i18n

--- frontend/src/components/auth/AuthLogout.tsx
+++ frontend/src/components/auth/AuthLogout.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "react"
 import { useAuth } from "react-oidc-context"
 import { IconLoader2 } from "@tabler/icons-react"
+import { useTranslation } from "react-i18next"
 
 export default function AuthLogout() {
   const auth = useAuth()
+  const { t } = useTranslation()
+  
 
   useEffect(() => {
     if (!auth.isLoading) {
@@ -14,9 +17,9 @@ export default function AuthLogout() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="text-center space-y-4">
+      <div className="flex flex-col gap-4 items-center justify-center bg-background h-128 w-lg shadow-xl">
         <IconLoader2 className="h-12 w-12 animate-spin mx-auto text-primary" />
-        <p className="text-lg">Signing out...</p>
+        <p className="text-lg">{t("Auth.Redirect.SignOut")}</p>
       </div>
     </div>
   )
