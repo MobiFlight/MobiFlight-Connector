9effa3cdd19caa47f5ff23fbe0c08bcaaa989492
Sebastian M <sebastian@mobiflight.com>
2026-02-23 02:08:12 +0100

    Add unit tests for controller binding

--- MobiFlight/Controllers/ControllerBinding.cs
+++ MobiFlight/Controllers/ControllerBinding.cs
@@ -8,6 +8,18 @@ namespace MobiFlight.Controllers
         public ControllerBindingStatus Status { get; set; }
         public Controller OriginalController { get; set; }
 
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = hash * 23 + (BoundController?.GetHashCode() ?? 0);
+                hash = hash * 23 + Status.GetHashCode();
+                hash = hash * 23 + (OriginalController?.GetHashCode() ?? 0);
+                return hash;
+            }
+        }
+
         public override bool Equals(object obj)
         {
             if (obj == null || GetType() != obj.GetType())
