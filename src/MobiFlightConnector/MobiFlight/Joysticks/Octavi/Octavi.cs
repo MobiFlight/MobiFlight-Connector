7021afe77dc90a66120f63e96610f40e84ffb9f8
FloH <doc.nice@gmx.de>
2026-04-20 23:39:32 +0200

    support shift mode without the need of another output rule

--- src/MobiFlightConnector/MobiFlight/Joysticks/Octavi/Octavi.cs
+++ src/MobiFlightConnector/MobiFlight/Joysticks/Octavi/Octavi.cs
@@ -66,9 +66,10 @@ namespace MobiFlight.Joysticks.Octavi
                     TriggerButtonPress(buttonIndex, inputEvent);
                 }
 
-                this.SetOutputDeviceState("Auto Blink Context", (byte)(octaviHandler.IsInShiftMode ? 1 : 0));
+                this.Update();
             }
         }
+
 
         protected override void SendData(byte[] data)
         {
@@ -95,6 +96,12 @@ namespace MobiFlight.Joysticks.Octavi
             });
         }
 
+        private void UpdateShiftModeLed()
+        {
+            /* this will set nothing if the Ouput device label is not found - disable auto blinking by renaming the output in joystick.json */
+            this.SetOutputDeviceState("Auto Blink Context", (byte)(octaviHandler.IsInShiftMode ? 1 : 0));
+        }
+
         public override void Update()
         {
             // Octavi is not a DirectInput device
@@ -103,9 +110,13 @@ namespace MobiFlight.Joysticks.Octavi
             {
                 Connect();
             };
+
             // We don't do anything else
             // because we have a callback for
             // handling the incoming reports
+            // only check for updated shift state and update the outputs if needed.
+            this.UpdateShiftModeLed();
+            this.UpdateOutputDeviceStates();
         }
 
         protected override void EnumerateDevices()
