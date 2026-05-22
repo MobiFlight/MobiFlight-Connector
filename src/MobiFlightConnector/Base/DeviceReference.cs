6d7abc1b4adb9a079c46f862b63e5de5f0dc83d4
Sebastian <sebastian@mobiflight.com>
2026-05-22 10:57:49 +0200

    Move DeviceReference to its own file

---
+++ src/MobiFlightConnector/Base/DeviceReference.cs
@@ -0,0 +1,50 @@
+﻿namespace MobiFlight.Base
+{
+    public class DeviceReference
+    {
+        public virtual string Name { get; set; }
+
+        // temporay property, will be removed before merging with the main branch
+        public string Label { get; set; }
+        public string SubId { get; set; }
+        public DeviceType Type { get; set; }
+
+        public DeviceReference() { }
+        public DeviceReference(DeviceType type, string name, string subId = null)
+        {
+            Type = type;
+            Name = name;
+            SubId = subId;
+        }
+
+        public virtual object Clone()
+        {
+            return new DeviceReference(Type, Name, SubId);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is DeviceReference reference))
+            {
+                return false;
+            }
+
+            return
+                   Type == reference.Type &&
+                   Name == reference.Name &&
+                   SubId == reference.SubId;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                hash = hash * 23 + Type.GetHashCode();
+                hash = hash * 23 + (Name?.GetHashCode() ?? 0);
+                hash = hash * 23 + (SubId?.GetHashCode() ?? 0);
+                return hash;
+            }
+        }
+    }
+}
--- src/MobiFlightConnector/MobiFlight/InputEventArgs.cs
+++ src/MobiFlightConnector/MobiFlight/InputEventArgs.cs
@@ -4,57 +4,6 @@ using System;
 
 namespace MobiFlight
 {
-    namespace Base
-    {
-        public class DeviceReference
-        {
-            public string Name { get; set; }
-
-            // temporay property, will be removed before merging with the main branch
-            public string Label { get; set; }
-            public string SubId { get; set; }
-            public DeviceType Type { get; set; }
-
-            public DeviceReference() { }
-            public DeviceReference(DeviceType type, string name, string subId = null)
-            {
-                Type = type;
-                Name = name;
-                SubId = subId;
-            }
-
-            public object Clone()
-            {
-                return new DeviceReference(Type, Name, SubId);
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (!(obj is DeviceReference reference))
-                {
-                    return false;
-                }
-
-                return
-                       Type == reference.Type &&
-                       Name == reference.Name &&
-                       SubId == reference.SubId;
-            }
-
-            public override int GetHashCode()
-            {
-                unchecked // Overflow is fine, just wrap
-                {
-                    int hash = 17;
-                    hash = hash * 23 + Type.GetHashCode();
-                    hash = hash * 23 + (Name?.GetHashCode() ?? 0);
-                    hash = hash * 23 + (SubId?.GetHashCode() ?? 0);
-                    return hash;
-                }
-            }
-        }
-    }
-
     public class InputEventArgs : EventArgs, ICloneable
     {
         public Controller Controller { get; set; }
