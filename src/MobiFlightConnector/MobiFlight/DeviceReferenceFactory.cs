913fde6b66d3c29db43f73540a843917ba9382f2
Sebastian <sebastian@mobiflight.com>
2026-05-22 22:28:42 +0200

    remove yield

--- src/MobiFlightConnector/MobiFlight/DeviceReferenceFactory.cs
+++ src/MobiFlightConnector/MobiFlight/DeviceReferenceFactory.cs
@@ -6,36 +6,38 @@ namespace MobiFlight
 {
     public static class DeviceReferenceFactory
     {
-        public static IEnumerable<DeviceReference> Create(BaseDevice dev)
+        public static List<DeviceReference> Create(BaseDevice dev)
         {
+            var result = new List<DeviceReference>();
             switch (dev.Type)
             {
                 case DeviceType.Button:
-                    yield return new DeviceReference { Name = dev.Name, Type = DeviceType.Button };
+                    result.Add(new DeviceReference { Name = dev.Name, Type = DeviceType.Button });
                     break;
 
                 case DeviceType.Encoder:
-                    yield return new DeviceReference { Name = dev.Name, Type = DeviceType.Encoder };
+                    result.Add(new DeviceReference { Name = dev.Name, Type = DeviceType.Encoder });
                     break;
 
                 case DeviceType.AnalogInput:
-                    yield return new DeviceReference { Name = dev.Name, Type = DeviceType.AnalogInput };
+                    result.Add(new DeviceReference { Name = dev.Name, Type = DeviceType.AnalogInput });
                     break;
 
                 case DeviceType.InputMultiplexer:
                     var mux = dev as InputMultiplexer;
                     int muxPinCount = int.TryParse(mux?.NumBytes, out int nb) ? nb * 8 : 16;
                     for (int i = 0; i < muxPinCount; i++)
-                        yield return new DeviceReference { Name = $"{dev.Name}:{i}", Type = DeviceType.Button };
+                        result.Add(new DeviceReference { Name = $"{dev.Name}:{i}", Type = DeviceType.Button });
                     break;
 
                 case DeviceType.InputShiftRegister:
                     var isr = dev as InputShiftRegister;
                     int isrPinCount = int.TryParse(isr?.NumModules, out int nm) ? nm * 8 : 8;
                     for (int i = 0; i < isrPinCount; i++)
-                        yield return new DeviceReference { Name = $"{dev.Name}:{i}", Type = DeviceType.Button };
+                        result.Add(new DeviceReference { Name = $"{dev.Name}:{i}", Type = DeviceType.Button });
                     break;
             }
+            return result;
         }
     }
 }
