4c38840179d780ffcb886a971ebd3882b0907b22
Sebastian <sebastian@mobiflight.com>
2026-06-05 20:23:15 +0200

    MSFS code can be edited by user

--- src/MobiFlightConnector/frontend/src/components/wizard/components/InputActions/MsfsInputActionPanel.tsx
+++ src/MobiFlightConnector/frontend/src/components/wizard/components/InputActions/MsfsInputActionPanel.tsx
@@ -3,6 +3,8 @@ import MsfsPresetPanel from "@/components/wizard/components/InputActions/MsfsPre
 import { MsfsInputAction } from "@/types/config"
 import { Label } from "@/components/ui/label"
 import { useTranslation } from "react-i18next"
+import { Separator } from "@/components/ui/separator"
+import { useState } from "react"
 
 export type MsfsInputActionPanelProps = {
   config: MsfsInputAction | null
@@ -14,7 +16,14 @@ const MsfsInputActionPanel = ({
   onConfigChange,
 }: MsfsInputActionPanelProps) => {
   const { t } = useTranslation()
-  const command = config?.Command ?? ""
+  const [ currentCommand, setCurrentCommand ] = useState(config?.Command ?? "")
+  const [ prevCommand, setPrevCommand ] = useState(config?.Command ?? "")
+
+  if (config?.Command !== prevCommand) {
+    setPrevCommand(config?.Command ?? "")
+    setCurrentCommand(config?.Command ?? "")
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <MsfsPresetPanel
@@ -28,15 +37,24 @@ const MsfsInputActionPanel = ({
           } as MsfsInputAction)
         }
       />
+      <Separator />
       <div className="flex flex-col gap-2">
         <Label htmlFor="code">{t("Dialog.InputConfigWizard.InputActions.Common.CodeLabel")}</Label>
         <Textarea
-        id="code"
-          value={
-            command
-              ? command
-              : t("Dialog.InputConfigWizard.InputActions.Msfs.NoneCode")
-          }
+          id="code"
+          placeholder={t("Dialog.InputConfigWizard.InputActions.Msfs.NoneCode")}
+          value={currentCommand}
+          onChange={(e) => {
+            setCurrentCommand(e.target.value)
+          }}
+
+          onBlur={() => {
+            onConfigChange({
+              ...(config as MsfsInputAction), 
+              Command: currentCommand,
+              PresetId: "", // Clear preset if user manually edits command
+            } as MsfsInputAction)
+          }}
         />
         <div className="text-sm text-muted-foreground">{t("Dialog.InputConfigWizard.InputActions.Common.SupportedPlaceholders")}</div>
       </div>
