8990900ae213ebc7c27c76e793e114a0586bbdaf
Sebastian <sebastian@mobiflight.com>
2026-06-25 13:50:09 +0200

    reduce number of parallel workers down to 2

--- src/MobiFlightConnector/frontend/playwright.config.ts
+++ src/MobiFlightConnector/frontend/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : 10,
+  workers: process.env.CI ? 2 : 10,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
