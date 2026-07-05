809d0fb5012eff26945e41fc2dfdfc942993694c
Sebastian <sebastian@mobiflight.com>
2026-07-05 12:09:38 +0200

    Using typed loglevel severity in settings object too

---
+++ tests/MobiFlightUnitTests/Base/SettingsTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MobiFlight.Base.Tests
+{
+    [TestClass]
+    public class SettingsTest
+    {
+        [TestMethod]
+        public void Settings_LogLevel_InvalidValue_ShouldDefaultToDebug()
+        {
+            // Arrange
+            var settings = new Properties.Settings
+            {
+                LogLevel = "InvalidLogLevel"
+            };
+            // Act
+            var mobiflightSettings = new Settings(settings);
+            // Assert
+            Assert.AreEqual(LogSeverity.Debug, mobiflightSettings.LogLevel);
+        }
+    }
+}
