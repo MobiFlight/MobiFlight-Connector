70b76c46c4cd8b3e8898b4cb4a67e0638ee65f59
Dark_Modest <darkmodest@hotmail.com>
2026-07-11 21:22:32 +0800

    feat(WelcomeDialog): add "Update" and "DoNotUpdate" buttons

--- src/MobiFlightConnector/UI/Dialogs/WelcomeDialog.Designer.cs
+++ src/MobiFlightConnector/UI/Dialogs/WelcomeDialog.Designer.cs
@@ -32,6 +32,8 @@ namespace MobiFlight.UI.Dialogs
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WelcomeDialog));
             this.okButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.doNotUpdateButton = new System.Windows.Forms.Button();
             this.titleLabel = new System.Windows.Forms.Label();
             this.panel = new System.Windows.Forms.Panel();
             this.openReleaseNotesinBrowserLabel = new System.Windows.Forms.Label();
@@ -46,6 +48,24 @@ namespace MobiFlight.UI.Dialogs
             this.okButton.Name = "okButton";
             this.okButton.UseVisualStyleBackColor = true;
             this.okButton.Click += new System.EventHandler(this.button1_Click);
+            //
+            // updateButton
+            //
+            this.updateButton.Location = new System.Drawing.Point(321, 2);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(125, 35);
+            this.updateButton.TabIndex = 3;
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            //
+            // doNotUpdateButton
+            //
+            this.doNotUpdateButton.Location = new System.Drawing.Point(452, 2);
+            this.doNotUpdateButton.Name = "doNotUpdateButton";
+            this.doNotUpdateButton.Size = new System.Drawing.Size(145, 35);
+            this.doNotUpdateButton.TabIndex = 4;
+            this.doNotUpdateButton.UseVisualStyleBackColor = true;
+            this.doNotUpdateButton.Click += new System.EventHandler(this.doNotUpdateButton_Click);
             // 
             // titleLabel
             // 
@@ -56,6 +76,8 @@ namespace MobiFlight.UI.Dialogs
             // 
             this.panel.Controls.Add(this.openReleaseNotesinBrowserLabel);
             this.panel.Controls.Add(this.okButton);
+            this.panel.Controls.Add(this.updateButton);
+            this.panel.Controls.Add(this.doNotUpdateButton);
             resources.ApplyResources(this.panel, "panel");
             this.panel.Name = "panel";
             // 
@@ -93,6 +115,8 @@ namespace MobiFlight.UI.Dialogs
 
         #endregion
         private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button doNotUpdateButton;
         private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.Panel panel;
         private Microsoft.Web.WebView2.WinForms.WebView2 webView;
