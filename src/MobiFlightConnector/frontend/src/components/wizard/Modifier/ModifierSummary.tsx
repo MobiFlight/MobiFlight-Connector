d28c79d7e2f066b259a19714418c0940e48c6e58
Sebastian <sebastian@mobiflight.com>
2026-07-15 07:41:08 +0200

    Adjust modifier summary

--- src/MobiFlightConnector/frontend/src/components/wizard/Modifier/ModifierSummary.tsx
+++ src/MobiFlightConnector/frontend/src/components/wizard/Modifier/ModifierSummary.tsx
@@ -17,32 +17,48 @@ const ModifierSummary = ({
 }: ModifierSummaryProps) => {
   const { t } = useTranslation()
   const rawValueClean = rawValue?.replace("CHANGE =>", "") ?? ""
+  const showMoreCount = modifiers.length > maxDisplayCount
+
   return (
-    <div className="flex flex-row items-center justify-between gap-2">
-      <Badge variant="secondary">{rawValueClean}</Badge>
-      <div className="mt-1 h-1 grow border-t border-dashed border-gray-800"></div>
-      <div className="flex flex-row flex-wrap gap-2">
-        {modifiers.slice(0, maxDisplayCount).map((modifier, index) => (
-          <div className="flex flex-row items-center gap-2" key={index}>
-            <Badge>{t(`Dialog.Modifiers.Type.${modifier.Type}.Label`)}</Badge>
-            <div className="mt-1 h-1 w-16 border-t border-dashed border-gray-800"></div>
-          </div>
-        ))}
-        {modifiers.length > maxDisplayCount && (
-          <Badge variant="secondary" className="border-primary">
-            {t("Dialog.Modifiers.Summary.More", {
-              count: modifiers.length - maxDisplayCount,
-            })}
-          </Badge>
-        )}
-        {modifiers.length === 0 && (
-          <Badge variant="secondary">
-            {t("Dialog.Modifiers.Summary.None")}
-          </Badge>
-        )}
+    <div className="flex flex-row items-end justify-between gap-2">
+      <div className="flex flex-col items-center gap-2">
+        <div className="text-sm font-medium">Raw Value</div>
+        <Badge variant="secondary">{rawValueClean}</Badge>
       </div>
-      <div className="mt-1 h-1 grow border-t border-dashed border-gray-800"></div>
-      <Badge variant="secondary">{finalValue}</Badge>
+
+      <div className="mt-1 h-3 grow border-t border-dashed border-gray-800"></div>
+
+      <div className="flex flex-row flex-wrap gap-2">
+        {modifiers.slice(0, maxDisplayCount).map((modifier, index) => {
+          const isLast =
+            index === modifiers.length - 1 || index === maxDisplayCount - 1
+          return (
+            <div className="flex flex-row items-center gap-2" key={index}>
+              <Badge variant="secondary">{t(`Dialog.Modifiers.Type.${modifier.Type}.Label`)}</Badge>
+              {!isLast && (
+                <div className="mt-1 h-1 w-16 border-t border-dashed border-gray-800"></div>
+              )}
+            </div>
+          )
+        })}
+      </div>
+      {showMoreCount && (
+        <div className="mt-1 h-3 w-16 border-t border-dashed border-gray-800"></div>
+      )}
+      {showMoreCount && (
+        <Badge variant="secondary" className="border-primary">
+          {t("Dialog.Modifiers.Summary.More", {
+            count: modifiers.length - maxDisplayCount,
+          })}
+        </Badge>
+      )}
+
+      <div className="mt-1 h-3 grow border-t border-dashed border-gray-800"></div>
+
+      <div className="flex flex-col items-center gap-2">
+        <div className="text-sm font-medium">Final Value</div>
+        <Badge variant="secondary">{finalValue}</Badge>
+      </div>
     </div>
   )
 }
