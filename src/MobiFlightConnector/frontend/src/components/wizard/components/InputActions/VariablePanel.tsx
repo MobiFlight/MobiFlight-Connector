82a326a74583fb56c52caec2c0b7ba63ff798f66
Omar Rashed <o.mar.om.123m@gmail.com>
2026-08-26 14:09:32 +0300

    fix size to be max-w-50 not w-auto

--- src/MobiFlightConnector/frontend/src/components/wizard/components/InputActions/VariablePanel.tsx
+++ src/MobiFlightConnector/frontend/src/components/wizard/components/InputActions/VariablePanel.tsx
@@ -145,7 +145,7 @@ export const VariablePanel = ({
                 )}
               </Label>
               <Input
-                className="w-auto"
+                className="max-w-50"
                 value={variable?.Name ?? ""}
                 onChange={(e) => {
                   onVariableChange({
