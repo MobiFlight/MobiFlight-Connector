bf123237188f9fa8f90413a51ada175d0bc75882
Jun Myung Ha <jh92@tu-clausthal.de>
2026-09-04 17:22:55 +0200

    done with backend part to send Shutdown request

---
+++ src/MobiFlightConnector/MobiFlight/BrowserMessages/Incoming/Handler/CommandShutdownHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MobiFlight.UI;
+
+namespace MobiFlight.BrowserMessages.Incoming.Handler
+{
+    public class CommandShutdownHandler
+    {
+        private readonly MainForm _mainForm;
+
+        public CommandShutdownHandler(MainForm mainForm)
+        {
+            _mainForm = mainForm;
+        }
+
+        public void Handle(CommandShutdown command)
+        {
+            if (command.Action == CommandShutdownAction.discardChanges)
+            {
+                _mainForm.confirmShutdownDiscardingChanges();
+            }
+        }
+    }
+}
---
+++ src/MobiFlightConnector/MobiFlight/BrowserMessages/Outgoing/ShutdownConfirmationRequested.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobiFlight.BrowserMessages.Outgoing
+{
+    internal class ShutdownConfirmationRequested
+    {
+    }
+}
